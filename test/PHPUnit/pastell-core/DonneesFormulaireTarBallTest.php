56d9a2527dbbcfffc6ae9f0aea8de85db79b65e1
Eric Pommateau <eric.pommateau@libriciel.coop>
2015-08-05 15:50:01 +0000

    Ajout d'un utilisataire pour données formulaire permettant de verser le contenu d'un élement tarball vers un élement de type fichier multiple.

---
+++ test/PHPUnit/pastell-core/DonneesFormulaireTarBallTest.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__.'/../init.php';
+
+class DonneesFormulaireTarBallTest extends PastellTestCase {
+	
+	public function reinitDatabaseOnSetup(){
+		return true;
+	}
+	
+	public function reinitFileSystemOnSetup(){
+		return true;
+	}
+	
+	
+	/**
+	 * @return DonneesFormulaire
+	 */
+	private function getDonneesFormulaire(){
+		return $this->getObjectInstancier()->DonneesFormulaireFactory->get('toto','test');
+	}
+	
+	public function testExtract(){
+		$donneesFormulaire = $this->getDonneesFormulaire();
+		$file_path = __DIR__."/../fixtures/tar_ball/courrier_simple.tar.gz";
+		$donneesFormulaire->addFileFromCopy('fichier_in', basename($file_path), $file_path);
+		
+		$donneesFormulaireTarBall = new DonneesFormulaireTarBall(new TmpFolder());
+		$donneesFormulaireTarBall->extract($donneesFormulaire, 'fichier_in', 'fichier_out');
+		
+		$this->assertEquals($donneesFormulaire->getFileName('fichier_out',0), "001-862614864-20150805-20150805B-AI-2-1_0.xml");
+		$this->assertEquals(3,count($donneesFormulaire->get('fichier_out')));
+		
+	}
+	
+}
+
