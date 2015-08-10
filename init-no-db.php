85bfab8578606ed1a4fd5e034dee412e05e3a437
Eric Pommateau <eric.pommateau@libriciel.coop>
2015-08-10 15:18:34 +0000

    Correction de l'initialisation pour que les tests phpunit ne tente plus d'établir une connexion à la "vrai" base de données.

---
+++ init-no-db.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once(__DIR__."/DefaultSettings.php");
+
+
+set_include_path(	__DIR__ . "/pastell-core/" . PATH_SEPARATOR .
+__DIR__ . "/lib/" . PATH_SEPARATOR .
+__DIR__ . "/model" . PATH_SEPARATOR .
+__DIR__ . "/controler" . PATH_SEPARATOR .
+__DIR__ . "/connecteur-type" . PATH_SEPARATOR .
+get_include_path()
+);
+
+if ( ! function_exists('pastell_autoload')) {
+	//PHPUnit est incompatible avec cette fonction d'autoload (warning + lancement d'exception)
+	//Note : c'est un peu à la one-again, il faudrait sans doute refactorer cette fonction pour qu'elle
+	//fonctionne dans tous les cas.
+	function pastell_autoload($class_name) {
+		@ $result = include($class_name . '.class.php');
+		if ( ! $result ){
+			//throw new Exception("Impossible de trouver $class_name");
+			return false;
+		}
+		return true;
+	}
+}
+
+spl_autoload_register('pastell_autoload');
+
+if(php_sapi_name() != "cli") {
+	ini_set("session.cookie_httponly", 1);
+	session_start();
+}
+
+if (! function_exists('apc_fetch')){
+	function apc_fetch(){}
+	function apc_store(){}
+}
+
+
+require_once( PASTELL_PATH . "/lib/util.php");
+require_once("Connecteur.class.php");
--- test/PHPUnit/init.php
+++ test/PHPUnit/init.php
@@ -19,4 +19,4 @@ require_once 'PastellTestCase.class.php';
 
 require_once "mikey179/vfsStream/src/main/php/org/bovigo/vfs/vfsStream.php";
 
-require_once(__DIR__."/../../init.php");
+require_once(__DIR__."/../../init-no-db.php");
