b649aca648dc7ed950be52ee0f53f09dc02cbcef
Eric Pommateau <eric.pommateau@libriciel.coop>
2015-11-30 16:22:27 +0000

    Mise à disposition d'une classe SimpleXMLWrapper pour encapsuler les appels au fonction de création de SimpleXMLElement (#HD 12066)

---
+++ lib/SimpleXMLWrapper.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class SimpleXMLWrapper {
+
+	private $load_option;
+
+	public function __construct(){
+		$this->setLoadOption(LIBXML_PARSEHUGE);
+	}
+
+	public function setLoadOption($load_option){
+		$this->load_option = $load_option;
+	}
+
+	public function loadFile($file_path){
+		$save = libxml_use_internal_errors(true);
+		$xml = simplexml_load_file($file_path,"SimpleXMLElement",$this->load_option);
+		if ($xml === false) {
+			$errors = $this->getErrorString();
+			libxml_use_internal_errors($save);
+			throw new Exception("Le fichier $file_path n'est pas un XML correct : " . $errors);
+		}
+		libxml_use_internal_errors($save);
+		return $xml;
+	}
+
+	public function loadString($data) {
+		$save = libxml_use_internal_errors(true);
+		$xml = simplexml_load_string($data,"SimpleXMLElement",$this->load_option);
+		if ($xml === false) {
+			$errors = $this->getErrorString();
+			libxml_use_internal_errors($save);
+			throw new Exception("XML incorrect : " . $errors);
+		}
+		libxml_use_internal_errors($save);
+		return $xml;
+	}
+
+	private function getErrorString() {
+		$errors = '';
+		foreach (libxml_get_errors() as $error) {
+			$errors .= $error->message . '(Lig' . $error->line . ',Col' . $error->column . ")\n";
+		}
+		return $errors;
+	}
+
+}
---
+++ test/PHPUnit/lib/SimpleXMLWrapperTest.php
@@ -0,0 +1,44 @@
+<?php
+
+class SimpleXMLWrapperTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var SimpleXMLWrapper
+	 */
+	private $simpleXMLWrapper;
+
+	public function setUp(){
+		$this->simpleXMLWrapper = new SimpleXMLWrapper();
+	}
+
+	public function testLoadBadString(){
+		$this->setExpectedException("Exception","XML incorrect");
+		$this->simpleXMLWrapper->loadString("foo");
+	}
+
+	public function testLoadString(){
+		$this->assertEquals("foo",$this->simpleXMLWrapper->loadString("<foo></foo>")->getName());
+	}
+
+	public function testLoadFile(){
+		$file_path = $this->getFilePath("<foo></foo>");
+		$xml = $this->simpleXMLWrapper->loadFile($file_path);
+		$this->assertEquals("foo",$xml->getName());
+	}
+
+	public function testLoadBadFile(){
+		$file_path = $this->getFilePath("foo");
+		$this->setExpectedException("Exception","Le fichier vfs://test/fichier.xml n'est pas un XML correct");
+		$this->simpleXMLWrapper->loadFile($file_path);
+	}
+
+	private function getFilePath($file_content){
+		org\bovigo\vfs\vfsStream::setup('test');
+		$testStreamUrl = org\bovigo\vfs\vfsStream::url('test');
+		$file_path = $testStreamUrl."/fichier.xml";
+		file_put_contents($file_path,$file_content);
+		return $file_path;
+	}
+
+
+}
