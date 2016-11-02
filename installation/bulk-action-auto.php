b81552ffea49d6cd36414a06f555c677bcab4b4d
Ines RUIZ <ines.ruiz@libriciel.coop>
2016-11-02 12:47:43 +0000

    report 1.4 -> 2.0: Ajout d'un script permettant d'inscrire en action auto tout plein de document d'un coup (r1862)

---
+++ installation/bulk-action-auto.php
@@ -0,0 +1,38 @@
+<?php
+require_once( __DIR__ . "/../init.php");
+
+
+if (count($argv) != 5){
+    echo "{$argv[0]} : Programme une action pour un ensemble de document\n";
+    echo "Usage : {$argv[0]} id_e type_document etat_source etat_cible\n";
+    exit;
+}
+
+$id_e = get_argv(1);
+$type = get_argv(2);
+$etat_source = get_argv(3);
+$etat_cible = get_argv(4);
+
+/** @var DocumentActionEntite $document */
+$document = $objectInstancier->getInstance('DocumentActionEntite');
+
+$document_list = $document->getDocument($id_e,$type,$etat_source);
+
+/** @var JobManager $jobManager */
+$jobManager =$objectInstancier->getInstance('JobManager');
+
+try {
+
+    foreach($document_list as $document_info) {
+        echo "{$document_info['id_d']} ({$document_info['titre']}): {$document_info['last_action']} -> $etat_cible\n";
+        $jobManager->setTraitementLot($id_e, $document_info['id_d'], 0, $etat_cible);
+    }
+
+
+} catch (Exception $e){
+    echo $e->getMessage()."\n";
+}
+
+
+
+
