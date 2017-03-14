df7b241fe4a2bf6c6be75863a4248c1b94cbdc65
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-03-14 16:25:30 +0000

    Correction d'un bug sous IE11 pour l'accès à la définition d'un rôle

--- template/RoleIndex.php
+++ template/RoleIndex.php
@@ -7,7 +7,7 @@
 		</tr>
 	<?php foreach($allRole as $i => $info) : ?>
 		<tr>
-			<td><a href='<?php $this->url("Role/detail?role={$info['role']}") ?>'><?php hecho($info['role']) ?></a></td>
+			<td><a href='<?php $this->url("Role/detail?role=".urlencode($info['role'])) ?>'><?php hecho($info['role']) ?></a></td>
 			<td><?php 
 			echo $info['libelle'] ?></td>
 		</tr>
