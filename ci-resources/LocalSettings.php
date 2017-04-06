54cbda6cfd122d71fadc3b7abe34caa67e64f142
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-04-06 16:24:22 +0200

    Ajout de bcmath

--- ci-resources/LocalSettings.php
+++ ci-resources/LocalSettings.php
@@ -3,3 +3,6 @@
 /* LocalSettings.php recopier à la racine par gitlab-ci */
 
 define("BD_DSN_TEST","mysql:dbname=pastell_test;host=mysql;port=3306;charset=utf8");
+define("BD_DBNAME_TEST","pastell_test");
+define("BD_USER_TEST","user");
+define("BD_PASS_TEST","user");
