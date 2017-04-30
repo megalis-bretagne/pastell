6eb857c6c9ff686398783086b2ad62797c33ecc2
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-04-30 07:33:50 +0200

    Ajout du LocalSettings si jamais il existe...

--- ci-resources/define-from-environnement.php
+++ ci-resources/define-from-environnement.php
@@ -1,5 +1,9 @@
 <?php
 
+//Utilisé dans le cadre de PHPStorm qui ne permet pas de lancer des scripts après le démarrage du Docker
+//et qui ne lance pas l'entrypoint
+//Uniquement utilisé pour PHPUnit et Codeception donc
+
 if (! file_exists(__DIR__."/../LocalSettings.php")) {
 	$script = __DIR__ . "/docker-pastell-init";
 
