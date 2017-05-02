8f355711cb396ca2745194f248f285a913ea90c7
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-05-02 08:50:06 +0200

    add var from gitlabci

--- ci-resources/init-docker.php
+++ ci-resources/init-docker.php
@@ -1,35 +1,38 @@
 <?php
 
+try {
 
-echo "Initialisation de Pastell [DOCKER]\n";
+    echo "Initialisation de Pastell [DOCKER]\n";
 
 # Première étape : Sans la connexion BD vu que celle-ci n'existe pas encore...
-require_once( __DIR__ . "/../init-no-db.php");
+    require_once(__DIR__ . "/../init-no-db.php");
 
-echo "Utilisation de la base ".BD_DSN." avec l'utilisateur ".BD_USER ."\n";
+    echo "Utilisation de la base " . BD_DSN . " avec l'utilisateur " . BD_USER . "\n";
 
-$objectInstancier = new ObjectInstancier();
-$sqlQuery = new SQLQuery(BD_DSN, BD_USER, BD_PASS);
-$databaseUpdate = new DatabaseUpdate(file_get_contents(__DIR__ . "/../installation/pastell.bin"),$sqlQuery);
-$databaseUpdate->majDatabase(
-    $sqlQuery,
-    function ($message) {
-        echo "[".date("Y-m-d H:i:s")."][Pastell SQL init] $message\n";
-    }
-);
+    $objectInstancier = new ObjectInstancier();
+    $sqlQuery = new SQLQuery(BD_DSN, BD_USER, BD_PASS);
+    $databaseUpdate = new DatabaseUpdate(file_get_contents(__DIR__ . "/../installation/pastell.bin"), $sqlQuery);
+    $databaseUpdate->majDatabase(
+        $sqlQuery,
+        function ($message) {
+            echo "[" . date("Y-m-d H:i:s") . "][Pastell SQL init] $message\n";
+        }
+    );
 
 # Deuxième étape : initialisation normal de Pastell
-require_once __DIR__ . "/../init.php";
+    require_once __DIR__ . "/../init.php";
 
-/** @var PastellBootstrap $pastellBootstrap */
-$pastellBootstrap = $objectInstancier->getInstance("PastellBootstrap");
+    /** @var PastellBootstrap $pastellBootstrap */
+    $pastellBootstrap = $objectInstancier->getInstance("PastellBootstrap");
 
-$envWrapper = new EnvWrapper();
-$utilisateurObject = new UtilisateurObject();
-$utilisateurObject->login = $envWrapper->get('PASTELL_ADMIN_LOGIN','admin');
-$utilisateurObject->password = $envWrapper->get('PASTELL_ADMIN_PASSWORD','admin');
-$utilisateurObject->email = $envWrapper->get('PASTELL_ADMIN_EMAIL','noreply@libriciel.coop');
+    $envWrapper = new EnvWrapper();
+    $utilisateurObject = new UtilisateurObject();
+    $utilisateurObject->login = $envWrapper->get('PASTELL_ADMIN_LOGIN', 'admin');
+    $utilisateurObject->password = $envWrapper->get('PASTELL_ADMIN_PASSWORD', 'admin');
+    $utilisateurObject->email = $envWrapper->get('PASTELL_ADMIN_EMAIL', 'noreply@libriciel.coop');
 
-$pastellBootstrap->bootstrap($utilisateurObject);
+    $pastellBootstrap->bootstrap($utilisateurObject);
 
-
+} catch (Exception $e){
+    echo $e->getMessage();
+}
