b185a23d3c4c01869ce52f88e3438785528a4f56
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-05-08 11:48:21 +0200

    On utilise plus la base de données pour codeception

--- test/codeception/tests/_bootstrap.php
+++ test/codeception/tests/_bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
 require_once __DIR__."/../../../ci-resources/define-from-environnement.php";
-require_once __DIR__."/../../../init.php";
 
-$result = $sqlQuery->query("DELETE FROM utilisateur");
+//require_once __DIR__."/../../../init.php";
 
-$result = $objectInstancier->AdminControler->createAdmin('admin', 'admin', 'eric.pommateau@libriciel.coop');
+/*$result = $sqlQuery->query("DELETE FROM utilisateur");
+
+$result = $objectInstancier->AdminControler->createAdmin('admin', 'admin', 'eric.pommateau@libriciel.coop');*/
 
