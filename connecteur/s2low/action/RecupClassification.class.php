da9018eca09e150e5faeb681ba3b07a3405da012
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-05-18 17:30:12 +0200

    Typage connecteur S2low

--- connecteur/s2low/action/RecupClassification.class.php
+++ connecteur/s2low/action/RecupClassification.class.php
@@ -3,6 +3,7 @@
 class RecupClassification extends ActionExecutor {
 	
 	public function go(){
+	    /** @var S2low $s2low */
 		$s2low = $this->getMyConnecteur();
 		$classification = $s2low->getClassification();
 		$this->getConnecteurProperties()->addFileFromData("classification_file","classification.xml",$classification);
