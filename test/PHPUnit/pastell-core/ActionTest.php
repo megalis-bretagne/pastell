2803041a4c3eb275c34ae424c92050862ccd26cd
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-05-18 14:43:25 +0200

    Ajout test unitaire ActionTest

---
+++ test/PHPUnit/pastell-core/ActionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+class ActionTest extends PHPUnit_Framework_TestCase {
+
+    /** @var  Action */
+    private $action;
+
+    protected function setUp() {
+        parent::setUp();
+        $yamlLoader = new YMLLoader(new MemoryCacheNone());
+        $array = $yamlLoader->getArray(__DIR__."/fixtures/definition-for-action-test.yml");
+        $this->action = new Action($array['action']);
+    }
+
+    public function testGetAll(){
+        $result = $this->action->getAll();
+        $this->assertEquals('modification',$result[0]);
+    }
+
+    public function testGetActionName(){
+        $this->assertEquals(
+            "En cours de rédaction",
+            $this->action->getActionName('modification')
+        );
+    }
+
+    public function testGetActionNameIdAction(){
+        $this->assertEquals(
+            'action-without-name',
+            $this->action->getActionName('action-without-name')
+        );
+    }
+
+    public function testGetActionNameFatalError(){
+        $this->assertEquals(
+            'Erreur fatale',
+            $this->action->getActionName('fatal-error')
+        );
+    }
+
+    public function testGetDoActionName(){
+        $this->assertEquals(
+            "Modifier",
+            $this->action->getDoActionName('modification')
+        );
+    }
+
+    public function testGetDoActionNameIdAction(){
+        $this->assertEquals(
+            "action-without-name",
+            $this->action->getDoActionName('action-without-name')
+        );
+    }
+
+    public function testGetActionRule(){
+        $result = $this->action->getActionRule('modification');
+        $this->assertEquals('test:lecture',$result['droit_id_u']);
+    }
+
+
+
+}
