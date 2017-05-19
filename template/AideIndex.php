f092f354148411194a42bcedbc962bd9b64dadc5
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-05-19 07:54:24 +0200

    Ajout d'un lien dans l'aide

---
+++ template/AideIndex.php
@@ -0,0 +1,9 @@
+
+<div class="alert-info alert">
+<p>Pour les clients <a href="https://www.libriciel.fr">Libriciel</a>, la documentation est disponible
+    en cliquant sur le lien ci-dessous :
+</p>
+<a href="https://otrs.libriciel.fr/otrs/customer.pl?Action=CustomerFAQExplorer;CategoryID=9">
+    Accéder à la documentation
+</a>
+</div>
