c6917fb1f7d041a2a9e4679afcdfc1b220ca308f
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-05-20 18:59:30 +0200

    Utilisation de phpseclib via composer

---
+++ test/PHPUnit/divers/HasComposerLoadedTest.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class HasComposerLoadedTest extends PHPUnit_Framework_TestCase {
+
+    public function testHasComposer(){
+        $mille = new \phpseclib\Math\BigInteger("1000",10);
+        $this->assertEquals("1000",$mille);
+
+    }
+}
