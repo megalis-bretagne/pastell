deb0d792c488bb13b42b8892f96c2cade001323b
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-05-24 07:44:10 +0200

    Ajout test entity

--- test/codeception/tests/api/EntiteCest.php
+++ test/codeception/tests/api/EntiteCest.php
@@ -2,6 +2,47 @@
 
 class EntiteCest {
 
-    
+    public function tryListEntite(NoGuy $I){
+        $I->wantTo("lister les entités auxquelles j'ai accès");
+        $I->amHttpAuthenticatedAsAdmin();
+        $I->sendGET("/entite");
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(
+            array(
+                array(
+                    "id_e"=>1,
+                    "denomination" => "Bourg-en-Bresse",
+                    "siren" => "000000000",
+                    "type" => "collectivite",
+                    "centre_de_gestion" => "0",
+                    "entite_mere" => "0"
+                )
+            )
+        );
+    }
+
+    public function tryListEntiteV1(NoGuy $I){
+        $I->wantTo("lister les entités auxquelles j'ai accès [V1]");
+        $I->amHttpAuthenticatedAsAdmin();
+        $I->sendGETV1("/list-entite.php");
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(
+            array(
+                array(
+                    "id_e"=>1,
+                    "denomination" => "Bourg-en-Bresse",
+                    "siren" => "000000000",
+                    "type" => "collectivite",
+                    "centre_de_gestion" => "0",
+                    "entite_mere" => "0"
+                )
+            )
+        );
+    }
+
+
+
 
 }
