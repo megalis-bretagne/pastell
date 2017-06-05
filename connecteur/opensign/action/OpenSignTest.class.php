ae54954bf12cb1a17bbe604856831e1a2952a5f6
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-06-05 15:03:42 +0200

    Ajout connecteur Liberhorodatage

---
+++ connecteur/opensign/action/OpenSignTest.class.php
@@ -0,0 +1,14 @@
+<?php
+require_once( __DIR__ . "/../OpenSign.class.php");
+
+class OpenSignTest extends ActionExecutor {
+	
+	public function go(){
+	    /** @var OpenSign $opensign */
+		$opensign = $this->getMyConnecteur();
+		$result = $opensign->test();
+		$this->setLastMessage("Connexion OpenSign OK: $result");
+		return true;
+	}
+	
+}
---
+++ connecteur/opensign/action/OpenSignTestVerifToken.class.php
@@ -0,0 +1,16 @@
+<?php
+require_once( __DIR__ . "/../OpenSign.class.php");
+
+class OpenSignTestVerifToken extends ActionExecutor {
+	
+	public function go(){
+        /** @var OpenSign $opensign */
+		$opensign = $this->getMyConnecteur();
+		$data = mt_rand(0,mt_getrandmax());
+		$token = $opensign->getTimestampReply($data);		
+		$opensign->verify($data,$token);
+		$this->setLastMessage("Vérification: OK");
+		return true;
+	}
+	
+}
