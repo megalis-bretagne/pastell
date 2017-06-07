c823617f589f3fb6b6d4d830d3996ed3f832af0e
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-06-07 11:23:25 +0200

    ptlc-tdt rev23
    Connecteur global S2low: Ajout d'un paramétre permettant de choisir le nom du flux actes utilisant la classification

--- connecteur/s2low/action/RecupClassificationAll.class.php
+++ connecteur/s2low/action/RecupClassificationAll.class.php
@@ -4,15 +4,21 @@ class RecupClassificationAll extends ActionExecutor {
 	
 	public function go(){
 
+		$connecteur_properties = $this->getConnecteurProperties();
+		$nom_flux_actes = $connecteur_properties->get('nom_flux_actes') ? $connecteur_properties->get('nom_flux_actes'):'actes-generique';
+
+
 		$entiteListe = new EntiteListe($this->getSQLQuery());
 		
 		$all_col = $entiteListe->getAll(Entite::TYPE_COLLECTIVITE);
 		$all_col =  array_merge($all_col,$entiteListe->getAll(Entite::TYPE_CENTRE_DE_GESTION));
-		
+		$all_col =  array_merge($all_col,$entiteListe->getAll(Entite::TYPE_SERVICE));
+
+
 		$envoye = array();
 		foreach($all_col as $infoCollectivite) {			
 			try {
-				$tdT = $this->objectInstancier->ConnecteurFactory->getConnecteurByType($infoCollectivite['id_e'],'actes-generique','TdT');
+				$tdT = $this->objectInstancier->ConnecteurFactory->getConnecteurByType($infoCollectivite['id_e'],$nom_flux_actes,'TdT');
 				if (!$tdT){
 					continue;
 				}
