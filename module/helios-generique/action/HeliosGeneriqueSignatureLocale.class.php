b40937fa1741be9f23991652bddfd2ba65c108ab
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-06-08 11:14:28 +0200

    ptl-helios rev55
    Correction d'un bug empechant le fonctionnement de Libersign V2 dans Firefox
    cf https://gitlab.libriciel.fr/i-parapheur/libersign-jquery/issues/2

--- module/helios-generique/action/HeliosGeneriqueSignatureLocale.class.php
+++ module/helios-generique/action/HeliosGeneriqueSignatureLocale.class.php
@@ -1,13 +1,6 @@
 <?php
 class HeliosGeneriqueSignatureLocale extends ChoiceActionExecutor {
 
-	/**
-	 * @return Libersign
-	 */
-	private function getLibersign(){
-		return $this->getConnecteur('signature');
-	}
-
 	public function go(){
 		$recuperateur = new Recuperateur($_POST);
 		$signature = $recuperateur->get("signature_1");
@@ -15,19 +8,18 @@ class HeliosGeneriqueSignatureLocale extends ChoiceActionExecutor {
 		if (! $signature){
 			throw new Exception("Aucune signature n'a été trouvée");
 		}
-		
+
 		$pes_filepath = $this->getDonneesFormulaire()->getFilePath("fichier_pes");
 		$pes_filename  = $this->getDonneesFormulaire()->getFileName("fichier_pes");
-
-        /** @var Libersign $libersign */
-        $libersign = $this->getConnecteur('signature');
+		/** @var Libersign $libersign */
+		$libersign = $this->getConnecteur('signature');
 		$new_pes_content = $libersign->injectSignaturePES($pes_filepath, $signature, $isbordereau);
-	
+
 		$this->getDonneesFormulaire()->addFileFromData('fichier_pes_signe', $pes_filename, $new_pes_content);
 		$this->getDonneesFormulaire()->setData('signature_link', "La signature a été recupérée");
-       
-        $this->getActionCreator()->addAction($this->id_e,$this->id_u,'recu-iparapheur',"La signature a été récupérée depuis l'applet de signature");
-		$this->notify('recu-iparapheur', $this->type,"La signature a été récupérée depuis l'applet de signature");       
+
+		$this->getActionCreator()->addAction($this->id_e,$this->id_u,'recu-iparapheur',"La signature a été récupérée depuis l'applet de signature");
+		$this->notify('recu-iparapheur', $this->type,"La signature a été récupérée depuis l'applet de signature");
 		$this->setLastMessage("La signature a été récupérée");
 
 		$this->redirect("/document/detail.php?id_e=".$this->id_e."&id_d=".$this->id_d."&page=".$this->page);
@@ -36,23 +28,25 @@ class HeliosGeneriqueSignatureLocale extends ChoiceActionExecutor {
 	public function displayAPI(){
 		throw new Exception("Cette fonctionnalité n'est pas disponible via l'API.");
 	}
-	
-	public function display(){
-		$libersign = $this->getLibersign();
-		$this->{'libersign_properties'} =  $this->getConnecteurConfigByType('signature');
-		$this->{'libersign_url'} = $libersign->getLibersignURL();
 
-			
+	public function display(){
+		$this->libersign_properties =  $this->getConnecteurConfigByType('signature');
+		//$this->libersign_url = $this->getConnecteurConfigByType('signature')->get("libersign_applet_url");
+
+
+		/** @var Libersign libersignConnecteur */
+		$this->libersignConnecteur = $this->getConnecteur('signature');
+
 		$pes_filepath = $this->getDonneesFormulaire()->getFilePath("fichier_pes");
 
-		$this->{'signatureInfo'} = $libersign->getInfoForSignaturePES($pes_filepath);
-		
+		$this->signatureInfo = $this->libersignConnecteur->getInfoForSignaturePES($pes_filepath);
+
 		$document_info = $this->getDocument()->getInfo($this->id_d);
-		$this->{'info'} = $document_info;
-		
+		$this->info = $document_info;
+
 		$type_name = $this->getDocumentTypeFactory()->getFluxDocumentType($this->type)->getName();
 
-        $this->renderPage("Signature du fichier PES - " .  $document_info['titre'] . " (".$type_name.")",__DIR__."/../template/HeliosSignatureLocale.php");
+		$this->renderPage("Signature du fichier PES - " .  $document_info['titre'] . " (".$type_name.")",__DIR__."/../template/HeliosSignatureLocale.php");
 	}
-	
+
 }
