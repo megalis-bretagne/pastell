a649b2e03e9ade0a8a68521d1e9eda4e0359499d
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-10-25 15:47:40 +0200

    Le connecteur GED est déprécié en grande partie

--- connecteur-type/GEDConnecteur.class.php
+++ connecteur-type/GEDConnecteur.class.php
@@ -1,25 +1,52 @@
 <?php 
 
 abstract class GEDConnecteur extends Connecteur {
-	abstract public function createFolder($folder,$title,$description);
-	abstract public function addDocument($title,$description,$contentType,$content,$gedFolder);
-	abstract public function getRootFolder();
-	abstract public function listFolder($folder);
-	public function sendDonneesForumulaire(DonneesFormulaire $donneesFormulaire){}
 
+    /** @var  DonneesFormulaire */
+    protected $connecteurConfig;
+
+    public function setConnecteurConfig(DonneesFormulaire $donneesFormulaire) {
+        $this->connecteurConfig = $donneesFormulaire;
+    }
+
+    public function send(DonneesFormulaire $donneesFormulaire){}
+
+
+    /** @deprecated */
+    public function sendDonneesForumulaire(DonneesFormulaire $donneesFormulaire){
+        $this->send($donneesFormulaire);
+    }
+
+    /** @deprecated */
+	public function createFolder($folder,$title,$description){}
+
+    /** @deprecated */
+    public function addDocument($title,$description,$contentType,$content,$gedFolder){}
+
+    /** @deprecated */
+    public function getRootFolder(){}
+
+    /** @deprecated */
+    public function listFolder($folder){}
+
+    /** @deprecated */
 	public function getSanitizeFolderName($folder){
 		$folder = strtr($folder," àáâãäçèéêëìíîïñòóôõöùúûüýÿ","_aaaaaceeeeiiiinooooouuuuyy");
 		$folder = preg_replace('/[^\w_]/',"",$folder);
 		return $folder;
 	}
 
+    /** @deprecated */
 	public function getSanitizeFileName($file){
 		$file = strtr($file," àáâãäçèéêëìíîïñòóôõöùúûüýÿ","_aaaaaceeeeiiiinooooouuuuyy");
 		$file = preg_replace('/[^\w-_\.]/',"",$file);
 		return $file;
 	}
 
+    /** @deprecated */
 	public function forceAddDocument($local_path, $path_on_server){}
+
+	/** @deprecated */
 	public function forceCreateFolder($new_folder_name){}
 }
 
