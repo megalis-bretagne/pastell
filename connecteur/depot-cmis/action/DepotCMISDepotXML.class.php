df5bb810d50a2ddcfebe3a475d5fbe6b73f9db3a
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-12-12 09:24:22 +0100

    Auto stash before merge of "dev-ep" and "origin/dev-ep"

---
+++ connecteur/depot-cmis/action/DepotCMISDepotXML.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class DepotCMISDepotXML extends ActionExecutor {
+
+    public function go(){
+        /** @var DepotConnecteur $depotConnecteur */
+        $depotConnecteur = $this->getMyConnecteur();
+
+        $filename = 'test_file_'. mt_rand(0,mt_getrandmax()).".xml";
+
+        $result = $depotConnecteur->saveDocument(
+            "",
+            $filename,
+            __DIR__ . "/../fixtures/test.xml"
+        );
+
+        $this->setLastMessage("Dépot du fichier sur : $result");
+        return true;
+    }
+}
