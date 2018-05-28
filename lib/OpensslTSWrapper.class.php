161a6453a7432065a7fba235f27efda5f61625e2
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2018-05-28 16:46:57 +0200

    ajout de la clé signer_digest pour la config tsa vérification du code de retour shell à la place du retour classique

--- lib/OpensslTSWrapper.class.php
+++ lib/OpensslTSWrapper.class.php
@@ -1,12 +1,12 @@
 <?php
 
 class OpensslTSWrapper {
-	
+
 	private $opensslPath;
 	private $lastError;
 
 	private $hash_algorithm;
-	
+
 	public function __construct($opensslPath){
 		$this->opensslPath = $opensslPath;
 		$this->setHashAlgorithm('sha1');
@@ -21,31 +21,31 @@ class OpensslTSWrapper {
 	public function getLastError(){
 		return $this->lastError;
 	}
-	
+
 	private function execute($command){
 		return shell_exec($command);
 	}
-	
+
 	private function getTmpFile($data = ""){
 		$file_path = sys_get_temp_dir()  . "/" . mt_rand();
   		file_put_contents($file_path,$data);
   		return $file_path;
 	}
-	
+
 	public function getTimestampQuery($data){
 		$dataFilePath = $this->getTmpFile($data);
 		$result = $this->execute($this->opensslPath." ts -query -{$this->hash_algorithm} -data $dataFilePath -cert");
 		unlink($dataFilePath);
 		return $result;
 	}
-	
+
 	public function getTimestampQueryString($timestampQuery){
 		$timestampQueryFilePath = $this->getTmpFile($timestampQuery);
 		$result =  $this->execute( $this->opensslPath . " ts -query -in $timestampQueryFilePath -text " );
 		unlink($timestampQueryFilePath);
 		return $result;
 	}
-	
+
 	public function getTimestampReplyString($timestampReply){
 		$timestampReplyFilePath = $this->getTmpFile($timestampReply);
 		$commande = $this->opensslPath . " ts -reply -in $timestampReplyFilePath -text " ;
@@ -53,42 +53,44 @@ class OpensslTSWrapper {
 		unlink($timestampReplyFilePath);
 		return $result;
 	}
-	
+
 
 	public function verify($data,$timestampReply, $CAFilePath, $certFilePath,$configFile){
 
 		$dataFilePath = $this->getTmpFile($data);
 		$timestampReplyFilePath = $this->getTmpFile($timestampReply);
-		
+
+		// redirect the output to /dev/null and get the shell return code
+        // 0 means everything worked out
 		$command =  $this->opensslPath ." ts -verify " .
-					" -data $dataFilePath " . 
-					" -in $timestampReplyFilePath " . 
-					" -CAfile $CAFilePath" . 
+					" -data $dataFilePath " .
+					" -in $timestampReplyFilePath " .
+					" -CAfile $CAFilePath" .
 					" -untrusted $certFilePath " .
 					" -config " . $configFile .
-					" 2>&1 ";
-		
+					" > /dev/null ; echo $?";
+
 		$result =  trim($this->execute( $command));
 		
 		unlink($dataFilePath);
 		unlink($timestampReplyFilePath);
 			
 		$this->lastError = $result;
-		return ($result == "Verification: OK");
+		return $result === "0";
 	}
-	
-	public function createTimestampReply($timestampRequest,$signerCertificate,$signerKey,$signerKeyPassword,$configFile){		
+
+	public function createTimestampReply($timestampRequest,$signerCertificate,$signerKey,$signerKeyPassword,$configFile){
 		$timestampRequestFile = $this->getTmpFile($timestampRequest);
 		$timestampReplyFile = $this->getTmpFile("");
-		
-		$command = $this->opensslPath . " ts -reply " . 
+
+		$command = $this->opensslPath . " ts -reply " .
 					" -queryfile $timestampRequestFile" .
-					" -signer " . $signerCertificate . 
-					" -inkey " . $signerKey . 
+					" -signer " . $signerCertificate .
+					" -inkey " . $signerKey .
 					" -passin pass:".$signerKeyPassword .
-					" -out $timestampReplyFile " . 
+					" -out $timestampReplyFile " .
 					" -config " . $configFile . " 2>&1";
-		
+
 		shell_exec($command);
 		//TODO vérifier le retour
 
@@ -96,6 +98,6 @@ class OpensslTSWrapper {
 		unlink($timestampRequestFile);
 		unlink($timestampReplyFile);
 		return $timestampReply;
-	}	
-	
+	}
+
 }
