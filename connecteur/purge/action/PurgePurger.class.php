ca09c76086815e5ac4d737d92ed89d3a12ba5857
Unknown <eric@sigmalis.com>
2018-06-12 15:15:08 +0200

    Connecteur de purge : on ne fait pas le traitement si l'action supression n'est pas possible #388
    Close #388

--- connecteur/purge/action/PurgePurger.class.php
+++ connecteur/purge/action/PurgePurger.class.php
@@ -3,35 +3,16 @@
 class PurgePurger extends ActionExecutor {
 
 
-
+	/**
+	 * @return bool
+	 * @throws Exception
+	 */
     public function go(){
         /** @var Purge $connecteur */
         $connecteur = $this->getMyConnecteur();
+        $connecteur->purger();
 
-        if (! $connecteur->isActif()){
-            $this->setLastMessage("Le connecteur n'est pas actif");
-            return true;
-        }
-        $document_list = $connecteur->listDocument();
-        $message = "Programmation de la purge des documents : ";
-        foreach($document_list as $document_info) {
-
-            $this->objectInstancier->Journal->add(
-                Journal::DOCUMENT_TRAITEMENT_LOT,
-                $this->id_e,
-                $document_info['id_d'],'supression',
-                "Programmation dans le cadre du connecteur de purge {$this->id_ce}");
-
-
-            $this->objectInstancier->getInstance(JobManager::class)->setTraitementLot(
-                $this->id_e,
-                $document_info['id_d'],
-                $this->id_u,
-                'supression'
-            );
-            $message .= get_hecho("{$document_info['id_d']} - {$document_info['titre']} - {$document_info['last_action_date']}") . "<br/>";
-        }
-
+        $this->setLastMessage($connecteur->getLastMessage());
         return true;
     }
 }
