62dbe4e54c184f84a4268f97dc953b5d7dc78d65
Pierre-Emmanuel VIVER <pierre-emmanuel.viver@libriciel.coop>
2018-06-13 15:50:21 +0200

    Adaptation test-last-job compatible nagios

--- installation/test-last-job.php
+++ installation/test-last-job.php
@@ -1,27 +1,33 @@
 <?php
 
 require_once( __DIR__ . "/../init.php");
+$modenagios=false;
+$message="OK";
+$retour=0;
 
-
+#print_r($argv);
+if( (isset($argv[1])) && ($argv[1] == 'nagios')) {
+        $modenagios=true;
+}
 
 $sql = " SELECT MAX(last_try) FROM job_queue WHERE next_try<now() AND nb_try > 0";
 
 $last_try = $sqlQuery->queryOne($sql);
 
 if (! $last_try){
-	//la job queue est vide
-	exit(0);
+        //la jiob queue est vide
+        $message="OK la job queue est vide";
 }
 
 $nb_second_since_last_try = time() - strtotime($last_try);
 
 if ($nb_second_since_last_try > 3600){
-	
-	mail(
-		ADMIN_EMAIL,
-		"Le démon Pastell semble arreté","Le démon sur le site ".SITE_BASE."/daemon semble arreté depuis plus d'une heure"
-	);
-	
+        mail(
+                ADMIN_EMAIL,
+                "Le démon Pastell semble arreté","Le démon sur le site ".SITE_BASE."/daemon semble arreté depuis plus d'une heure"
+        );
+        $message="Le démon Pastell semble arreté sur le site ".SITE_BASE."/daemon semble arreté depuis plus d'une heure";
+        $retour=2;
 }
 
 
@@ -29,10 +35,16 @@ if ($nb_second_since_last_try > 3600){
 $jobQueueSQL = $objectInstancier->getInstance("JobQueueSQL");
 $nb_lock = $jobQueueSQL->getNbLockSinceOneHour();
 if ($nb_lock){
-	mail(
-		ADMIN_EMAIL,
-		"Des connecteurs Pastell sont verrouillés",
-		"$nb_lock connecteur(s) Pastell semble verrouillés sur le site ".SITE_BASE." depuis plus d'une heure !"
-	);
-	return;
+        mail(
+                ADMIN_EMAIL,
+                "Des connecteurs Pastell sont verrouillés",
+                "$nb_lock connecteur(s) Pastell semble verrouillés sur le site ".SITE_BASE." depuis plus d'une heure !"
+        );
+        $message="Des connecteurs Pastell, ".SITE_BASE.", sont verrouillés depuis plus d'une heure !";
+        $retour=2;
+}
+
+if($modenagios){
+        echo $message;
+        exit($retour);
 }
