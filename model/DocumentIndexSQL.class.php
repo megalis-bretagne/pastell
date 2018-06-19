1e791c7ca72c450743c28e6d90265218512b09ef
Unknown <eric@sigmalis.com>
2018-06-19 09:14:11 +0200

    Refactoring DocumentIndexSQL pour prendre en compte la longeur de la clé

--- model/DocumentIndexSQL.class.php
+++ model/DocumentIndexSQL.class.php
@@ -1,25 +1,41 @@
 <?php
 class DocumentIndexSQL extends SQL {
-	
-	public function index($id_d,$fieldName,$fieldValue){
+
+	const FIELD_VALUE_LENGTH = 128;
+	const FIELD_NAME_LENGTH = 128;
+
+	public function index($id_d, $field_name, $field_value){
+		$field_name = substr($field_name,0,self::FIELD_NAME_LENGTH);
+
 		$sql = "SELECT count(*) FROM document_index WHERE id_d=? AND field_name=?";
-		if ($this->queryOne($sql,$id_d,$fieldName)){
+		if ($this->queryOne($sql,$id_d,$field_name)){
 			$sql = "UPDATE document_index SET field_value = ? WHERE id_d=? AND field_name = ?";
-			$this->query($sql,$fieldValue,$id_d,$fieldName);
+			$this->query($sql,$field_value,$id_d,$field_name);
 		} else {
 			$sql = "INSERT INTO document_index(id_d,field_name,field_value) VALUES(?,?,?)";
-			$this->query($sql,$id_d,$fieldName,$fieldValue);
+			$this->query($sql,$id_d,$field_name,$field_value);
 		}
 	}
 	
-	public function get($id_d,$fieldName){
+	public function get($id_d, $field_name){
+		$field_name = $this->fieldNameSubstring($field_name);
 		$sql = "SELECT field_value FROM document_index WHERE id_d=? AND field_name=?";
-		return $this->queryOne($sql,$id_d,$fieldName);
+		return $this->queryOne($sql,$id_d,$field_name);
 	}
 	
-	public function getByFieldValue($fieldName, $fieldValue){
+	public function getByFieldValue($field_name, $field_value){
+		$field_name = $this->fieldNameSubstring($field_name);
+		$field_value = $this->fieldValueSubstring($field_value);
 		$sql = "SELECT id_d FROM document_index WHERE field_name=? AND field_value=?";
-		return $this->queryOne($sql,$fieldName, $fieldValue);
+		return $this->queryOne($sql,$field_name, $field_value);
+	}
+
+	private function fieldNameSubstring($field_name){
+		return substr($field_name,0,self::FIELD_NAME_LENGTH);
+	}
+
+	private function fieldValueSubstring($field_value){
+		return substr($field_value,0,self::FIELD_VALUE_LENGTH);
 	}
 
 }
--- test/PHPUnit/model/DocumentIndexSQLTest.php
+++ test/PHPUnit/model/DocumentIndexSQLTest.php
@@ -26,4 +26,31 @@ class DocumentIndexSQLTest extends PastellTestCase {
 		$this->assertEquals("FOO",$this->documentIndexSQL->getByFieldValue("bar","baz"));
 	}
 
+	public function testSaveBigField(){
+		$value = str_repeat("01234567890",20);
+		$this->documentIndexSQL->index('long',"my_big_field_value",$value);
+		$this->assertEquals(
+			substr($value,0,DocumentIndexSQL::FIELD_VALUE_LENGTH),
+			$this->documentIndexSQL->get('long',"my_big_field_value")
+		);
+		$this->assertEquals("long",
+			$this->documentIndexSQL->getByFieldValue('my_big_field_value',$value)
+		);
+	}
+
+	public function testSaveBigFieldName(){
+		$name = str_repeat("01234567890",20);
+		$this->documentIndexSQL->index('long',$name,"my_value");
+		$this->assertEquals(
+			"my_value",
+			$this->documentIndexSQL->get('long',$name)
+		);
+		$this->assertEquals(
+			"long",
+			$this->documentIndexSQL->getByFieldValue($name,"my_value")
+		);
+	}
+
+
+
 }
