8b35807f0177b0ddcdbc1eb065c01d5eab68ddac
Eric Pommateau <eric.pommateau@libriciel.coop>
2018-07-24 14:46:20 +0200

    Correction mail glaneur locale

--- connecteur/glaneur-local/action/GlaneurLocalVerifErreur.class.php
+++ connecteur/glaneur-local/action/GlaneurLocalVerifErreur.class.php
@@ -12,17 +12,22 @@ class GlaneurLocalVerifErreur extends ActionExecutor {
 			->getInstance(ConnecteurEntiteSQL::class)
 			->getAllById('glaneur-local');
 
-		$data = "";
+		$data = "Nombre de documents par connecteurs:\n";
 
 		$nb_ok = 0;
 		$nb_ko = 0;
 
+
+
 		foreach($all_connecteur as $connecteur){
-			$data .= $connecteur['denomination']." ".$connecteur['libelle']." : ";
+			if ($connecteur['id_e'] == 0){
+				continue;
+			}
+			$data .= $connecteur['denomination']." - ".$connecteur['libelle']." : ";
 			/** @var GlaneurLocal $glaneurLocal */
 			$glaneurLocal = $this->getConnecteurFactory()->getConnecteurById($connecteur['id_ce']);
 			$error_file = $glaneurLocal->listErrorDirectories();
-			$data.= count($error_file)."<br/>";
+			$data.= count($error_file)."\n";
 			if (count($error_file)){
 				$nb_ko++;
 			} else {
@@ -30,17 +35,19 @@ class GlaneurLocalVerifErreur extends ActionExecutor {
 			}
 		}
 
+		$data= "Nombre de connecteur ok : $nb_ok\nNombre de connecteur avec des erreurs : $nb_ko\n\n".$data;
+
 		if ($nb_ko){
 			mail(
 				ADMIN_EMAIL,
 				"[Pastell] Des glaneurs ont glanés des fichiers en erreurs",
 				$data
 			);
-			$data.="<br/><br/> mail envoyé à ".ADMIN_EMAIL;
+			$data.="\n\n mail envoyé à ".ADMIN_EMAIL;
 		}
 
-		$data= "Nombre de connecteur ok : $nb_ok<br/>Nombre de connecteur avec des erreurs : $nb_ko</br><br/>".$data;
-		$this->setLastMessage($data);
+
+		$this->setLastMessage(nl2br($data));
 		return ($nb_ko==0);
 
 	}
