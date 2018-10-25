dd88c6b8ce242cf6c16e6973e8142d0e3ca53164
iruiz <ines.ruiz@libriciel.coop>
2018-10-25 09:55:51 +0200

    Actions des connecteur-type: mise à jour de SignatureEnvoie et SignatureRecuperation #484

--- connecteur-type/signature/SignatureEnvoie.class.php
+++ connecteur-type/signature/SignatureEnvoie.class.php
@@ -15,13 +15,15 @@ class SignatureEnvoie extends ConnecteurTypeActionExecutor {
 		$has_date_limite = $this->getMappingValue('iparapheur_has_date_limite');
 		$iparapheur_date_limite = $this->getMappingValue('iparapheur_date_limite');
 		$annexe_element = $this->getMappingValue('autre_document_attache');
+        $primo_signature_detachee = $this->getMappingValue('primo_signature_detachee');
+        $json_metadata = $this->getMappingValue('json_metadata');
+        $iparapheur_dossier_id = $this->getMappingValue('iparapheur_dossier_id');
+
 
         $signature->setSendingMetadata($donneesFormulaire);
 
 		$file_content = $donneesFormulaire->getFileContent($document_element);
 		$content_type = $donneesFormulaire->getContentType($document_element);
-		$filename = $donneesFormulaire->getFileName($document_element);
-
 
 		$annexe = array();
 		if ($donneesFormulaire->get($annexe_element)) {
@@ -37,8 +39,33 @@ class SignatureEnvoie extends ConnecteurTypeActionExecutor {
 			}
 		}
 
-		$dossierID = $signature->getDossierID($donneesFormulaire->get($objet_element),$filename);
-		if ($donneesFormulaire->get($has_date_limite)){
+        $primo_signature_detachee_content = "";
+        $primo_signature_detachee_type = "";
+        if ($donneesFormulaire->get($primo_signature_detachee)) {
+            $primo_signature_detachee_content = $donneesFormulaire->getFileContent($primo_signature_detachee);
+            $primo_signature_detachee_type = $donneesFormulaire->getContentType($primo_signature_detachee);
+            if (!($primo_signature_detachee_type == 'application/xml')) {
+                $primo_signature_detachee_type = 'application/pkcs7-signature';
+            }
+        }
+
+        $dossierTitre = $donneesFormulaire->get($objet_element);
+
+        $metadata = $donneesFormulaire->getFileContent($json_metadata);
+        $metadata = json_decode($metadata,true);
+
+        if ($donneesFormulaire->getFormulaire()->getField($iparapheur_dossier_id)) {
+            $dossierID = date("YmdHis").mt_rand(0,mt_getrandmax());
+            $donneesFormulaire->setData($iparapheur_dossier_id,$dossierID);
+        }
+        else { // conservé pour compatibilité
+            $filename = $donneesFormulaire->getFileName($document_element);
+            $dossierID = $signature->getDossierID($donneesFormulaire->get($objet_element),$filename);
+        }
+
+        $signature->setSendingMetadata($donneesFormulaire);
+
+        if ($donneesFormulaire->get($has_date_limite)){
 			$date_limite = $donneesFormulaire->get($iparapheur_date_limite);
 		} else {
 			$date_limite = false;
@@ -51,13 +78,23 @@ class SignatureEnvoie extends ConnecteurTypeActionExecutor {
 			$file_content,
 			$content_type,
 			$annexe,
-			$date_limite
+			$date_limite,
+            "",
+            false,
+            "",
+            "",
+            "",
+            $metadata,
+            $dossierTitre,
+            $primo_signature_detachee_content,
+            $primo_signature_detachee_type
 		);
 		if (! $result){
 			$this->setLastMessage("La connexion avec le parapheur a échoué : " . $signature->getLastError());
 			return false;
 		}
 		$this->addActionOK("Le document a été envoyé au parapheur électronique");
+        $this->notify($this->action, $this->type,"Le document a été envoyé au parapheur électronique");
 		return true;
 	}
 
--- connecteur-type/signature/SignatureRecuperation.class.php
+++ connecteur-type/signature/SignatureRecuperation.class.php
@@ -10,64 +10,81 @@ class SignatureRecuperation extends ConnecteurTypeActionExecutor {
 		}
 
 		$donneesFormulaire = $this->getDonneesFormulaire();
+
 		$document_element = $this->getMappingValue('document');
 		$titre_element = $this->getMappingValue('titre');
 		$has_historique_element = $this->getMappingValue('has_historique');
 		$iparapheur_historique_element = $this->getMappingValue('iparapheur_historique');
+        $has_signature_element = $this->getMappingValue('has_signature');
+        $signature_element = $this->getMappingValue('signature');
+        $document_orignal_element = $this->getMappingValue('document_orignal');
+        $bordereau_element = $this->getMappingValue('bordereau');
+        $annexe_element = $this->getMappingValue('autre_document_attache');
+        $iparapheur_annexe_sortie_element = $this->getMappingValue('iparapheur_annexe_sortie');
+        $iparapheur_dossier_id = $this->getMappingValue('iparapheur_dossier_id');
 
-		$filename = $donneesFormulaire->getFileName($document_element);
-		$dossierID = $signature->getDossierID($donneesFormulaire->get($titre_element),$filename);
-		$all_historique = false;
-		try {
-			$all_historique = $signature->getAllHistoriqueInfo($dossierID);
-		} catch(Exception $e){
-			$this->throwError($signature, $e->getMessage());
-		}
 
-		if (! $all_historique){
-			$message = "La connexion avec le iParapheur a échoué : " . $signature->getLastError();
-			$this->throwError($signature, $message);
-		}
+        if ($donneesFormulaire->getFormulaire()->getField($iparapheur_dossier_id)) {
+            $dossierID = $donneesFormulaire->get($iparapheur_dossier_id);
+        }
+        else { // conservé pour compatibilité
+            $filename = $donneesFormulaire->getFileName($document_element);
+            $dossierID = $signature->getDossierID($donneesFormulaire->get($titre_element),$filename);
+        }
 
-		$array2XML = new Array2XML();
-		$historique_xml = $array2XML->getXML('iparapheur_historique',json_decode(json_encode($all_historique),true));
+        $erreur = false;
+        $all_historique = array();
+        try {
+            $all_historique = $signature->getAllHistoriqueInfo($dossierID);
+            if (! $all_historique){
+                $erreur = "La connexion avec le iParapheur a échoué : " . $signature->getLastError();
+            }
+        } catch (Exception $e){
+            $erreur = $e->getMessage();
+        }
 
-		$donneesFormulaire->setData($has_historique_element,true);
-		$donneesFormulaire->addFileFromData($iparapheur_historique_element,"iparapheur_historique.xml",$historique_xml);
+        if (! $erreur) {
+            $array2XML = new Array2XML();
+            $historique_xml = $array2XML->getXML($iparapheur_historique_element, json_decode(json_encode($all_historique), true));
 
-		$result = $signature->getLastHistorique($all_historique);
+            $donneesFormulaire->setData($has_historique_element,true);
+            $donneesFormulaire->addFileFromData($iparapheur_historique_element, "iparapheur_historique.xml", $historique_xml);
+            $result = $signature->getLastHistorique($all_historique);
+        } else {
+            $result = false;
+        }
 
-		if (strstr($result,"[Archive]")){
-			return $this->retrieveDossier($dossierID);
-		} else if (strstr($result,"[RejetVisa]") || strstr($result,"[RejetSignataire]")){
-			$this->rejeteDossier($dossierID,$result);
-		} else {
-			$this->throwError($signature, $result);
-		}
-		$this->setLastMessage($result);
-		return true;
+        if (strstr($result,"[Archive]")){
+            return $this->retrieveDossier($dossierID, $has_signature_element, $signature_element, $document_element, $document_orignal_element, $annexe_element, $iparapheur_annexe_sortie_element, $bordereau_element);
+        } else if (strstr($result,"[RejetVisa]") || strstr($result,"[RejetSignataire]")){
+            $this->rejeteDossier($dossierID, $result, $bordereau_element);
+            $this->setLastMessage($result);
+            return true;
+        }
+
+        $nb_jour_max = $signature->getNbJourMaxInConnecteur();
+        $lastAction = $this->getDocumentActionEntite()->getLastActionInfo($this->id_e,$this->id_d);
+        $time_action = strtotime($lastAction['date']);
+        if (time() - $time_action > $nb_jour_max * 86400){
+            $erreur = "Aucune réponse disponible sur le parapheur depuis $nb_jour_max jours !";
+            $this->getActionCreator()->addAction($this->id_e,$this->id_u,'erreur-verif-iparapheur',$erreur);
+            $this->notify('erreur-verif-iparapheur', $this->type,$erreur);
+        }
+
+        if (! $erreur){
+            $this->setLastMessage($result);
+            return true;
+        }
+
+        $this->setLastMessage($erreur);
+        return false;
+
 	}
 
-	public function throwError(SignatureConnecteur $signature,$message){
-		$nb_jour_max = $signature->getNbJourMaxInConnecteur();
-		$lastAction = $this->getDocumentActionEntite()->getLastActionInfo($this->id_e,$this->id_d);
-		$time_action = strtotime($lastAction['date']);
-
-		if (time() - $time_action > $nb_jour_max * 86400){
-			$message = "Aucune réponse disponible sur le parapheur depuis $nb_jour_max jours !";
-			$this->getActionCreator()->addAction($this->id_e,$this->id_u,'erreur-verif-iparapheur',$message);
-			$this->notify($this->action, $this->type,$message);
-		}
-
-		throw new Exception($message);
-	}
-
-	public function rejeteDossier($dossierID,$result){
+	public function rejeteDossier($dossierID,$result,$bordereau_element){
         /** @var SignatureConnecteur $signature */
         $signature = $this->getConnecteur('signature');
         $donneesFormulaire = $this->getDonneesFormulaire();
-
-        $bordereau_element = $this->getMappingValue('bordereau');
 
         $info = $signature->getSignature($dossierID);
         if (! $info ){
@@ -75,23 +92,26 @@ class SignatureRecuperation extends ConnecteurTypeActionExecutor {
             return false;
         }
         $donneesFormulaire->addFileFromData($bordereau_element,$info['nom_document'],$info['document']);
-
         $signature->effacerDossierRejete($dossierID);
 
-		$this->getActionCreator()->addAction($this->id_e,$this->id_u,'rejet-iparapheur',"Le document a été rejeté dans le parapheur : $result");
-		return true;
+        $message = "Le document a été rejeté dans le parapheur : $result";
+        $this->getActionCreator()->addAction($this->id_e,$this->id_u,'rejet-iparapheur',$message);
+        $this->notify($this->action, $this->type,$message);
+
 	}
 
-	public function retrieveDossier($dossierID){
+	public function retrieveDossier($dossierID,
+                                    $has_signature_element,
+                                    $signature_element,
+                                    $document_element,
+                                    $document_orignal_element,
+                                    $annexe_element,
+                                    $iparapheur_annexe_sortie_element,
+                                    $bordereau_element
+                                    ){
 		/** @var IParapheur $signature */
 		$signature = $this->getConnecteur('signature');
 		$donneesFormulaire = $this->getDonneesFormulaire();
-
-		$has_signature_element = $this->getMappingValue('has_signature');
-		$signature_element = $this->getMappingValue('signature');
-		$document_element = $this->getMappingValue('document');
-		$document_orignal_element = $this->getMappingValue('document_orignal');
-		$bordereau_element = $this->getMappingValue('bordereau');
 
 		$info = $signature->getSignature($dossierID,false);
 		if (! $info ){
@@ -103,13 +123,20 @@ class SignatureRecuperation extends ConnecteurTypeActionExecutor {
 		if ($info['signature']){
 			$donneesFormulaire->addFileFromData($signature_element,"signature.zip",$info['signature']);
 		}
+        elseif ($info['document_signe']['document']){
+            $document_original_name = $donneesFormulaire->getFileName('document');
+            $document_original_data = $donneesFormulaire->getFileContent('document');
+            $donneesFormulaire->addFileFromData($document_orignal_element, $document_original_name, $document_original_data);
 
-		$document_original_name = $donneesFormulaire->getFileName($document_element);
-		$document_original_data = $donneesFormulaire->getFileContent($document_element);
-		$donneesFormulaire->addFileFromData($document_orignal_element, $document_original_name, $document_original_data);
-		if (isset($info['document_signe']['document'])){
-			$donneesFormulaire->addFileFromData($document_element,$document_original_name,$info['document_signe']['document']);
-		}
+            $filename = substr($donneesFormulaire->getFileName($document_element), 0, -4);
+            $filename_signe = preg_replace("#[^a-zA-Z0-9_]#", "_", $filename)."_signe.pdf";
+            $donneesFormulaire->addFileFromData($document_element,$filename_signe,$info['document_signe']['document']);
+        }
+
+        $output_annexe = $signature->getOutputAnnexe($info,$donneesFormulaire->getFileNumber($annexe_element));
+        foreach ($output_annexe as $i => $annexe){
+            $donneesFormulaire->addFileFromData($iparapheur_annexe_sortie_element,$annexe['nom_document'],$annexe['document'],$i);
+        }
 
 		$donneesFormulaire->addFileFromData($bordereau_element,$info['nom_document'],$info['document']);
 
@@ -119,9 +146,9 @@ class SignatureRecuperation extends ConnecteurTypeActionExecutor {
             );
         }
 
-		$this->setLastMessage("La signature a été récupérée");
-
-		$this->getActionCreator()->addAction($this->id_e,$this->id_u,'recu-iparapheur',"La signature a été récupérée sur parapheur électronique");
-		return true;
+        $this->setLastMessage("La signature a été récupérée");
+        $this->notify('recu-iparapheur', $this->type,"La signature a été récupérée");
+        $this->getActionCreator()->addAction($this->id_e,$this->id_u,'recu-iparapheur',"La signature a été récupérée sur parapheur électronique");
+        return true;
 	}
 }
