7a5680a208a67fca92e622f202a0293d1498cf61
Eric Pommateau <eric.pommateau@libriciel.coop>
2018-11-26 16:17:59 +0100

    Connecteur i-Parapheur : Ajout d'une action pour vider le cache WSDL #464

---
+++ connecteur/iParapheur/action/IParapheurEmptyWSDLCache.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class IParapheurEmptyWSDLCache extends ActionExecutor {
+
+	/**
+	 *
+	 * On ne fait pas dans la dentelle : tous les fichiers WSDL sont supprimés !
+	 * L'extension SOAP ne permet pas de savoir quel est le fichier a supprimé
+	 * On aurait pu mettre la fonction sur le connecteur global, mais cela aurait rendu l'utilisation plus complexe
+	 *
+	 * @return bool
+	 */
+	public function go(){
+		$finder = new \Symfony\Component\Finder\Finder();
+		$filesystem = new \Symfony\Component\Filesystem\Filesystem();
+
+		$wsdl_cache_dir = ini_get("soap.wsdl_cache_dir");
+
+		$cacheFileIterator = $finder
+			->in($wsdl_cache_dir)
+			->name("wsdl-*")
+			->getIterator();
+
+		foreach($cacheFileIterator as $file){
+			$filesystem->remove($file);
+		}
+
+		$this->setLastMessage("Le cache WSDL a été supprimé");
+		return true;
+	}
+
+}
