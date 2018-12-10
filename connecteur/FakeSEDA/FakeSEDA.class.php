5ad35e3912239c6028719cb4809bfb276558ebfa
Eric Pommateau <eric.pommateau@libriciel.coop>
2018-12-10 10:07:40 +0100

    Correction de FakeSEDA pour qu'il fonctionne avec SEDANG

--- connecteur/FakeSEDA/FakeSEDA.class.php
+++ connecteur/FakeSEDA/FakeSEDA.class.php
@@ -10,4 +10,20 @@ class FakeSEDA extends SEDAConnecteur {
 		return file_get_contents(__DIR__."/fixtures/bordereau.xml");
 	}
 
+	public function getBordereauNG(FluxData $fluxData){
+		return $this->getBordereau([]);
+	}
+
+	public function validateBordereau(string $bordereau){
+		return true;
+	}
+
+	public function getLastValidationError(){
+		return [];
+	}
+
+	public function generateArchive(FluxData $fluxData,string $archive_path){
+		touch($archive_path);
+	}
+
 }
--- connecteur/seda-ng/lib/FluxDataStandard.class.php
+++ connecteur/seda-ng/lib/FluxDataStandard.class.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(__DIR__."/FluxData.class.php");
+
 class FluxDataStandard extends FluxData {
 
 	protected $donneesFormulaire;
