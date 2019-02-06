d06e078eca4a25edb95cd3e678fd15df85c49bea
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-02-06 18:19:22 +0100

    db

--- installation/pastell.sql
+++ installation/pastell.sql
@@ -297,6 +297,11 @@ CREATE TABLE `role_droit` (
 	`droit` varchar(64) NOT NULL,
 	PRIMARY KEY (`role`,`droit`)
 )  ENGINE=MyISAM  ;
+CREATE TABLE `type_dossier` (
+	`id_t` int(11) NOT NULL AUTO_INCREMENT,
+	`id_type_dossier` varchar(32) NOT NULL,
+	PRIMARY KEY (`id_t`)
+)  ENGINE=InnoDB  ;
 CREATE TABLE `utilisateur` (
 	`id_u` int(11) NOT NULL AUTO_INCREMENT,
 	`email` varchar(128) NOT NULL,
