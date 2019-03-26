cf0bee0377061c68a2482b48776342f5814606b8
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-03-26 15:22:26 +0100

    add choice of parapheur fast circuit

--- module/actes-generique/action/EnvoieSignatureChangeAPI.class.php
+++ module/actes-generique/action/EnvoieSignatureChangeAPI.class.php
@@ -1,10 +1,12 @@
 <?php
-class EnvoieSignatureChangeAPI extends ActionExecutor{
-	
-	public function go(){
-				
-		$this->getDonneesFormulaire()->setData('envoi_signature_check', $this->getDonneesFormulaire()->get('envoi_signature'));	
-	}
-	
-	
+
+class EnvoieSignatureChangeAPI extends ActionExecutor {
+
+    public function go() {
+        $this->getDonneesFormulaire()->setData(
+            'envoi_signature_check',
+            $this->getDonneesFormulaire()->get('envoi_signature')
+            || $this->getDonneesFormulaire()->get('envoi_signature_fast')
+        );
+    }
 }
