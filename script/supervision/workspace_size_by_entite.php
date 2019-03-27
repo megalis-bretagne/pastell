cc7f05707dc0397de0492689d0bf66e95f187e51
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-03-27 10:37:09 +0100

    Ajout d'un script supervision/workspace_size_by_entite.php permettant d'obtenir la taille des documents par entité #663

---
+++ script/supervision/workspace_size_by_entite.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(__DIR__ . "/../../init.php");
+
+$entiteSQL = $objectInstancier->getInstance(EntiteSQL::class);
+
+$documentEntite = $objectInstancier->getInstance(DocumentEntite::class);
+
+$sql = "SELECT id_d FROM document_entite WHERE id_e=?";
+
+$entite_list = $entiteSQL->getAll();
+
+foreach($entite_list as $entite_info){
+	$id_d_list = $sqlQuery->queryOneCol($sql,$entite_info['id_e']);
+	$size = 0;
+	foreach($id_d_list as $id_d){
+		$a = $id_d[0];
+		$b = $id_d[1];
+		$all_file = glob(WORKSPACE_PATH."/{$a}/{$b}/{$id_d}*");
+
+		array_walk($all_file,function($filepath) use (& $size){
+			$size += filesize($filepath);
+		});
+	}
+
+	echo "{$entite_info['id_e']};{$entite_info['denomination']};$size\n";
+
+}
+
+
+
+
