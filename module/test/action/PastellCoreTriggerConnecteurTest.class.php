70b268446d01ac82159ea832be6d8f55598a8066
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-03-28 14:47:29 +0100

    Ajout action pour le connecteur de test

---
+++ module/test/action/PastellCoreTriggerConnecteurTest.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class PastellCoreTriggerConnecteurTest extends ActionExecutor {
+
+	/**
+	 * @return bool
+	 * @throws Exception
+	 */
+	public function go(){
+		/** @var TestConnecteur $testConnecteur */
+		$testConnecteur = $this->getConnecteur("test");
+		$champs1 = $testConnecteur->getChamps1();
+		$this->setLastMessage("Le champs 1 du connecteur contient $champs1");
+		return true;
+	}
+
+}
