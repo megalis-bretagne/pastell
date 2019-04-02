54fd3e37a4c0f24cf5d1776fa4bcc2c710bd6ba2
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-04-02 16:28:00 +0200

    send document and its annexes

--- module/actes-generique/action/FastParapheurEnvoi.class.php
+++ module/actes-generique/action/FastParapheurEnvoi.class.php
@@ -18,13 +18,29 @@ class FastParapheurEnvoi extends ActionExecutor
 
         $file_content = file_get_contents($file_path);
 
+        $annexe = [];
+
+        $annexe_field_key = 'autre_document_attache';
+        if ($acte->get($annexe_field_key)) {
+            foreach ($acte->get($annexe_field_key) as $num => $fileName) {
+                $filePath = $acte->getFilePath($annexe_field_key, $num);
+                $annexe_content = file_get_contents($filePath);
+
+                $annexe[] = [
+                    'name' => $fileName,
+                    'file_content' => $annexe_content,
+                    'file_path' => $filePath
+                ];
+            }
+        }
+
         $result = $signature->sendDocument(
             $filename,
             $acte->get('fast_parapheur_circuit'),
-            '',
+            $file_path,
             $file_content,
             '',
-            []);
+            $annexe);
 
         if (!$result) {
             $this->setLastMessage("La connexion avec le parapheur a échouée : " . $signature->getLastError());
