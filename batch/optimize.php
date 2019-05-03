55c7bab0bd55b9b4c458e2bd1d6701111f3026c9
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-05-03 16:34:07 +0200

    change optimize time script and optimize the journal

--- batch/optimize.php
+++ batch/optimize.php
@@ -3,7 +3,7 @@
 
 //Script à passer tous les jours pour éviter des problèmes de performance et de corruption de la table worker
 
-require_once( dirname(__FILE__) . "/../init.php");
+require_once dirname(__FILE__) . "/../init.php";
 
 /** @var SQLQuery $sqlQuery */
 $sqlQuery = $objectInstancier->getInstance('SQLQuery');
@@ -12,9 +12,10 @@ $list = array(
     "OPTIMIZE TABLE worker",
     "ANALYZE TABLE worker",
     "OPTIMIZE TABLE job_queue",
-    "ANALYZE TABLE job_queue"
+    "ANALYZE TABLE job_queue",
+    "OPTIMIZE TABLE journal",
 );
-foreach($list as $command) {
+foreach ($list as $command) {
     echo "$command:...";
     $sqlQuery->query($command);
     echo "[OK]\n";
