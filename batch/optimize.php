5205462cadff3fa096561503d4535d39ea8f9bc7
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-05-09 17:01:55 +0200

    remove unneeded analyze table

--- batch/optimize.php
+++ batch/optimize.php
@@ -3,16 +3,14 @@
 
 //Script à passer tous les jours pour éviter des problèmes de performance et de corruption de la table worker
 
-require_once dirname(__FILE__) . "/../init.php";
+require_once __DIR__ . "/../init.php";
 
 /** @var SQLQuery $sqlQuery */
 $sqlQuery = $objectInstancier->getInstance('SQLQuery');
 
 $list = array(
     "OPTIMIZE TABLE worker",
-    "ANALYZE TABLE worker",
     "OPTIMIZE TABLE job_queue",
-    "ANALYZE TABLE job_queue",
     "OPTIMIZE TABLE journal",
 );
 foreach ($list as $command) {
