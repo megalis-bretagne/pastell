a74e76d9e7b122cd4f69cd5853fe8ebeb675a75d
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-05-24 14:25:32 +0200

    add mail notifications to convocations flux if the user is subscribed to mailsec notifications

---
+++ script/bug/add-mailsec-notifications-to-convocation-flux.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'init.php';
+
+$notification_source = 'mailsec';
+$notification_target = 'convocation';
+
+$utilisateurList = $objectInstancier->getInstance(UtilisateurListe::class);
+$notification = $objectInstancier->getInstance(Notification::class);
+
+$mailActions = ['envoi-mail', 'reception', 'reception-partielle', 'renvoi'];
+$users = $utilisateurList->getAllUtilisateurSimple();
+
+foreach ($users as $user) {
+    $userId = $user['id_u'];
+    $userNotifications = $notification->getAll($userId);
+
+    foreach ($userNotifications as $userNotification) {
+        if ($userNotification['type'] === $notification_source) {
+            foreach ($mailActions as $action) {
+                $notification->add(
+                    $userId,
+                    $userNotification['id_e'],
+                    $notification_target,
+                    $action,
+                    $userNotification['daily_digest']
+                );
+            }
+        }
+    }
+}
