68fa4c9227aec1d5ec7f1c3f04c941fe4afd9a7c
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-05-27 16:00:29 +0200

    add flux name in parameter

--- script/bug/add-mailsec-notifications-to-flux.php
+++ script/bug/add-mailsec-notifications-to-flux.php
@@ -2,8 +2,15 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'init.php';
 
+if (count($argv) != 2){
+    echo "{$argv[0]} : Ajoute les notifications mails du flux choisi aux utilisateurs déjà abonnés aux notifications mail sécurisé \n";
+    echo "Usage : {$argv[0]} flux_cible\n";
+    echo "Exemple : {$argv[0]} convocation\n";
+    exit;
+}
+
 $notification_source = 'mailsec';
-$notification_target = 'convocation';
+$notification_target = get_argv(1);
 
 $utilisateurList = $objectInstancier->getInstance(UtilisateurListe::class);
 $notification = $objectInstancier->getInstance(Notification::class);
