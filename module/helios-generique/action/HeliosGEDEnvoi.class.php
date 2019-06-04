2efdcdf9ff191612f9de607d932c163cecdcf151
iruiz <ines.ruiz@libriciel.coop>
2019-06-04 10:33:42 +0200

    ajout de require GEDEnvoyer pour les classe d'envoi GED deprecated #716

--- module/helios-generique/action/HeliosGEDEnvoi.class.php
+++ module/helios-generique/action/HeliosGEDEnvoi.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once (__DIR__."/../../../connecteur-type/GED/GEDEnvoyer.class.php");
 
 /** @deprecated PA 3.0 utiliser connecteur-type/GEDEnvoyer à la place */
 class HeliosGEDEnvoi extends GEDEnvoyer { /** Nothing to do */}
