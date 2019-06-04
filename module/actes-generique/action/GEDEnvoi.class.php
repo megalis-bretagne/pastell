8ea7833cde8db949bc4a634527eafbb112fd9985
iruiz <ines.ruiz@libriciel.coop>
2019-06-04 14:56:26 +0200

    ajout de require GEDEnvoyer pour les classe d'envoi GED deprecated - relecture sup parenthèses #716

--- module/actes-generique/action/GEDEnvoi.class.php
+++ module/actes-generique/action/GEDEnvoi.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once (__DIR__."/../../../connecteur-type/GED/GEDEnvoyer.class.php");
+require_once __DIR__."/../../../connecteur-type/GED/GEDEnvoyer.class.php";
 
 /** @deprecated PA 3.0 utiliser connecteur-type/GEDEnvoyer à la place */
 class GEDEnvoi extends GEDEnvoyer { /** Nothing to do */}
