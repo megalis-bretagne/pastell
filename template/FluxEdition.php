95721c303812358cf8099fcb0d9b6ef99f32dc55
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-06-18 15:21:48 +0200

    #735 - 2

--- template/FluxEdition.php
+++ template/FluxEdition.php
@@ -94,6 +94,6 @@ aucun connecteur sélectionné
     <a class='btn btn-secondary' href='Flux/index?id_e=<?php echo $id_e?>'>
         <i class="fa fa-times-circle"></i>&nbsp;Annuler
     </a>
-<button type='submit' class='btn btn-primary'><i class='fa fa-link'></i>&nbsp;Associer</button>
+<button type='submit' class='btn btn-primary'><i class='fa fa-floppy-o'></i>&nbsp;Enregistrer</button>
 </form>
 </div>
