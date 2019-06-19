5ddfb931b8aa033a6fc53642ac5a077b1b71e7ad
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-06-19 16:55:07 +0200

    #635

--- controler/EntiteControler.class.php
+++ controler/EntiteControler.class.php
@@ -407,6 +407,8 @@ class EntiteControler extends PastellControler {
 		$this->{'droit_edition'}= $this->getRoleUtilisateur()->hasDroit($this->getId_u(),"entite:edition",$id_e);
 		$this->{'id_e'}= $id_e;
 		$this->{'all_connecteur'}= $this->getConnecteurEntiteSQL()->getAll($id_e);
+		$this->{'all_connecteur_definition'} =
+			$this->getObjectInstancier()->getInstance(ConnecteurDefinitionFiles::class)->getAll();
 		$this->{'template_milieu'}= "ConnecteurList";
 		$this->{'menu_gauche_select'} = "Entite/connecteur";
 		$this->setPageTitle("Liste des connecteurs".($id_e?"":" globaux"));
