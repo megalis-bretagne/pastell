94d0d1c0bf2d257a259f549b7f1271b1784c1ecd
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-07-09 14:58:49 +0200

    #762

--- template/AideIndex.php
+++ template/AideIndex.php
@@ -1,9 +1,9 @@
 
 <div class="alert-info alert">
-<p>Pour les clients <a href="https://www.libriciel.fr">Libriciel</a>, la documentation est disponible
+<p>Pour les clients <a href="https://www.libriciel.fr" target="_blank">Libriciel</a>, la documentation est disponible
     en cliquant sur le lien ci-dessous :
 </p>
-<a href="https://otrs.libriciel.fr/otrs/customer.pl?Action=CustomerFAQExplorer;CategoryID=9">
+<a href="https://otrs.libriciel.fr/otrs/customer.pl?Action=CustomerFAQExplorer;CategoryID=9" target="_blank">
     Accéder à la documentation
 </a>
 </div>
