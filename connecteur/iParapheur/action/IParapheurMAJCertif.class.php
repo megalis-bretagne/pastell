36bc5e6eba7d43c13c92228e1c373ae84c42edd5
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-07-10 14:04:37 +0200

    Refactorring + correction

--- connecteur/iParapheur/action/IParapheurMAJCertif.class.php
+++ connecteur/iParapheur/action/IParapheurMAJCertif.class.php
@@ -2,12 +2,15 @@
 class IParapheurMAJCertif extends ChoiceActionExecutor {
 
 
+	/**
+	 * @return bool
+	 * @throws Exception
+	 */
     public function go(){
-        $recuperateur = new Recuperateur($_POST);
-
-        $go =$recuperateur->get('go');
+        $go = $this->getRecuperateur()->get('go');
         if (! $go){
             $this->display();
+            return true;
         }
 
         return $this->updateCertificate();
@@ -19,41 +22,56 @@ class IParapheurMAJCertif extends ChoiceActionExecutor {
 
     public function display(){
         $all_connecteur = array();
-        foreach($this->objectInstancier->ConnecteurEntiteSQL->getAllById('iParapheur') as $connecteur){
+        $connecteur_list = $this->objectInstancier
+			->getInstance(ConnecteurEntiteSQL::class)
+			->getAllById('iParapheur');
+        foreach($connecteur_list as $connecteur){
             if (! $connecteur['id_e']){
                 continue;
             }
             $all_connecteur[] = $connecteur;
         }
 
-        $this->all_connecteur = $all_connecteur;
+        $this->{'all_connecteur'} = $all_connecteur;
 
-        $this->renderPage("Mise à jour certificat iParapheur", __DIR__."/../template/IParapheurChoixMAJCertificat.php");
+        $this->renderPage(
+        	"Mise à jour certificat iParapheur",
+			__DIR__."/../template/IParapheurChoixMAJCertificat.php"
+		);
         return true;
     }
 
+	/**
+	 * @return bool
+	 * @throws Exception
+	 */
     private function updateCertificate(){
-        $recuperateur = new Recuperateur($_POST);
-        $id_ce_list = $recuperateur->get('id_ce_list');
+        $id_ce_list = $this->getRecuperateur()->get('id_ce_list');
         if (!$id_ce_list){
-            throw new Exception("Aucun connecteur sélectionné");
+            throw new UnrecoverableException("Aucun connecteur sélectionné");
         }
 
         $fileUploader = new FileUploader();
         $user_certificate = $fileUploader->getFileContent('user_certificat');
 
         if (! $user_certificate){
-            throw new Exception("Il faut sélectionné un certificat");
+            throw new UnrecoverableException("Il faut sélectionner un certificat");
         }
 
         foreach($id_ce_list as $id_ce){
             $connecteurConfig = $this->getConnecteurFactory()->getConnecteurConfig($id_ce);
             if($user_certificate){
                 $user_certificate_name = $fileUploader->getName('user_certificat');
-                $user_certificat_password = $recuperateur->get('user_certificat_password');
-                $connecteurConfig->addFileFromData('iparapheur_user_certificat', $user_certificate_name, $user_certificate);
+                $user_certificat_password = $this->getRecuperateur()->get('user_certificat_password');
+                $connecteurConfig->addFileFromData(
+                	'iparapheur_user_certificat',
+					$user_certificate_name,
+					$user_certificate
+				);
                 $connecteurConfig->setData('iparapheur_user_certificat_password', $user_certificat_password);
-                $this->objectInstancier->ActionExecutorFactory->executeOnConnecteur($id_ce,$this->id_u,'update-certificate');
+                $this->objectInstancier
+					->getInstance(ActionExecutorFactory::class)
+					->executeOnConnecteur($id_ce,$this->id_u,'update-certificate');
             }
         }
 
