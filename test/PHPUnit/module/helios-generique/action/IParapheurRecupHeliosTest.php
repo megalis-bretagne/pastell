12a87b6e0fe4d2e6b3cd947e1f00c341ae9130be
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-07-17 16:12:56 +0200

    use connector type for pes signature

--- test/PHPUnit/module/helios-generique/action/IParapheurRecupHeliosTest.php
+++ test/PHPUnit/module/helios-generique/action/IParapheurRecupHeliosTest.php
@@ -2,11 +2,55 @@
 
 class IParapheurRecupHeliosTest extends PastellTestCase
 {
+    /**
+     * @param PHPUnit_Framework_MockObject_MockObject $soapClient
+     */
+    private function mockSoapClientFactory(PHPUnit_Framework_MockObject_MockObject $soapClient): void
+    {
+        $soapClientFactory = $this->getMockBuilder(SoapClientFactory::class)->getMock();
+        $soapClientFactory
+            ->expects($this->any())
+            ->method('getInstance')
+            ->willReturn($soapClient);
+
+        $this->getObjectInstancier()->setInstance(SoapClientFactory::class, $soapClientFactory);
+    }
+
+    private function createAndAssociateIparapheurConnector(): void
+    {
+        $connector = $this->createConnector('iParapheur', 'parapheur');
+        $this->configureConnector($connector['id_ce'], [
+            'iparapheur_activate' => true,
+            'iparapheur_wsdl' => 'wsdl'
+        ]);
+        $this->associateFluxWithConnector($connector['id_ce'], 'helios-generique', 'signature');
+    }
+
+    /**
+     * @param DonneesFormulaire $donneesFormulaire
+     * @return DonneesFormulaire
+     */
+    private function setDefaultDataToDocument(DonneesFormulaire $donneesFormulaire): DonneesFormulaire
+    {
+        $donneesFormulaire->setData('objet', 'PES document');
+        $donneesFormulaire->addFileFromCopy(
+            'fichier_pes',
+            'pes.xml',
+            __DIR__ . '/../fixtures/HELIOS_SIMU_ALR2_1496987735_826268894.xml'
+        );
+        $donneesFormulaire->addFileFromCopy(
+            'visuel_pdf',
+            'visuel.pdf',
+            __DIR__ . '/../../../fixtures/vide.pdf'
+        );
+
+        return $donneesFormulaire;
+    }
 
     /**
      * @throws NotFoundException
      */
-    public function testGetSignedPes()
+    public function testWhenModifyingDocumentIdAfterBeingSent()
     {
         $soapClient = $this->getMockBuilder(SoapClient::class)->disableOriginalConstructor()->getMock();
         $soapClient
@@ -32,35 +76,14 @@ class IParapheurRecupHeliosTest extends PastellTestCase
                 }
             ));
 
-        $soapClientFactory = $this->getMockBuilder(SoapClientFactory::class)->getMock();
-        $soapClientFactory
-            ->expects($this->any())
-            ->method('getInstance')
-            ->willReturn($soapClient);
+        $this->mockSoapClientFactory($soapClient);
 
-        $this->getObjectInstancier()->setInstance(SoapClientFactory::class, $soapClientFactory);
-
-        $connector = $this->createConnector('iParapheur', 'parapheur');
-        $this->configureConnector($connector['id_ce'], [
-            'iparapheur_activate' => true,
-            'iparapheur_wsdl' => 'wsdl'
-        ]);
-        $this->associateFluxWithConnector($connector['id_ce'], 'helios-generique', 'signature');
+        $this->createAndAssociateIparapheurConnector();
 
         $document = $this->createDocument('helios-generique');
 
         $donneesFormulaire = $this->getDonneesFormulaireFactory()->get($document['id_d']);
-        $donneesFormulaire->setData('objet', 'PES document');
-        $donneesFormulaire->addFileFromCopy(
-            'fichier_pes',
-            'pes.xml',
-            __DIR__ . '/../fixtures/HELIOS_SIMU_ALR2_1496987735_826268894.xml'
-        );
-        $donneesFormulaire->addFileFromCopy(
-            'visuel_pdf',
-            'visuel.pdf',
-            __DIR__ . '/../../../fixtures/vide.pdf'
-        );
+        $donneesFormulaire = $this->setDefaultDataToDocument($donneesFormulaire);
 
         $this->triggerActionOnDocument($document['id_d'], 'send-iparapheur');
         $this->assertLastMessage('Le document a été envoyé au parapheur électronique');
@@ -76,4 +99,57 @@ class IParapheurRecupHeliosTest extends PastellTestCase
          */
         $this->assertLastMessage('01/01/1970 01:00:00 : [status] annotation');
     }
+
+    /**
+     * @throws NotFoundException
+     */
+    public function testThatDocumentIdIsUsedWhenRequestingItOnParapheur()
+    {
+        $this->createAndAssociateIparapheurConnector();
+        $document = $this->createDocument('helios-generique');
+        $id_d = $document['id_d'];
+        $donneesFormulaire = $this->getDonneesFormulaireFactory()->get($id_d);
+        $this->setDefaultDataToDocument($donneesFormulaire);
+
+        $soapClient = $this->getMockBuilder(SoapClient::class)->disableOriginalConstructor()->getMock();
+        $soapClient
+            ->expects($this->any())
+            ->method('__call')
+            ->willReturnCallback(
+                function ($soapMethod, $arguments) use ($id_d) {
+                    if ($soapMethod === 'GetHistoDossier') {
+                        $this->assertSame(
+                            $this->getDonneesFormulaireFactory()->get($id_d)->get('iparapheur_dossier_id'),
+                            $arguments[0]
+                        );
+
+                        return json_decode(json_encode([
+                            'LogDossier' => [
+                                [
+                                    'timestamp' => 1,
+                                    'annotation' => 'annotation',
+                                    'status' => 'status'
+
+                                ]
+                            ]
+                        ]), false);
+                    }
+                    return json_decode('{"MessageRetour":{"codeRetour":"OK","message":"message.","severite":"INFO"}}', false);
+                }
+            );
+
+        $this->mockSoapClientFactory($soapClient);
+
+        $this->triggerActionOnDocument($document['id_d'], 'send-iparapheur');
+        $this->assertLastMessage('Le document a été envoyé au parapheur électronique');
+        $this->triggerActionOnDocument($document['id_d'], 'verif-iparapheur');
+
+        /**
+         * If this assertion fails with "La connexion avec le iParapheur a échoué : Failed asserting that two strings are identical."
+         * It probably means that the assertion in the returnCallback() of the mocked soapClient is broken and the exception
+         * is caught by the connector.
+         */
+        $this->assertLastMessage('01/01/1970 01:00:00 : [status] annotation');
+    }
+
 }
