60d83b01293946727d46398586cf96d8efc37055
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-07-18 11:27:18 +0200

    Refactoring

--- module/document-a-signer/action/IParapheurEnvoieDocumentASigner.class.php
+++ module/document-a-signer/action/IParapheurEnvoieDocumentASigner.class.php
@@ -1,54 +1,9 @@
 <?php
 
-class IParapheurEnvoieDocumentASigner extends ActionExecutor {
-	
-	public function go(){
+require_once __DIR__."/../../../connecteur-type/signature/SignatureEnvoie.class.php";
 
-	    /** @var IParapheur $signature */
-		$signature = $this->getConnecteur('signature');
-				
-		$donneesFormulaire = $this->getDonneesFormulaire();
-
-        $signature->setSendingMetadata($donneesFormulaire);
-
-
-        $file_content = $donneesFormulaire->getFileContent('document');
-		$content_type = $donneesFormulaire->getContentType('document');
-		$filename = $donneesFormulaire->getFileName('document');
-
-        $autre_doc = array();
-        if ($donneesFormulaire->get('autre_document_attache')) {
-            foreach($donneesFormulaire->get('autre_document_attache') as $num => $fileName ){
-                $autre_doc_content =  $donneesFormulaire->getFileContent('autre_document_attache',$num);
-                $autre_doc_content_type = $donneesFormulaire->getContentType('autre_document_attache',$num);
-
-                $autre_doc[] = array(
-                    'name' => $fileName,
-                    'file_content' => $autre_doc_content,
-                    'content_type' => $autre_doc_content_type,
-                );
-            }
-        }
-
-        $dossierID = $signature->getDossierID($donneesFormulaire->get('libelle'),$filename);
-		if ($donneesFormulaire->get('has_date_limite')){
-			$date_limite = $donneesFormulaire->get('date_limite');
-		} else {
-			$date_limite = false;
-		}
-		
-		$result = $signature->sendDocument($donneesFormulaire->get('iparapheur_type'),
-									$donneesFormulaire->get('iparapheur_sous_type'),
-									$dossierID,
-									$file_content,
-									$content_type,$autre_doc,$date_limite);
-		if (! $result){
-			$this->setLastMessage("La connexion avec le iParapheur a échoué : " . $signature->getLastError());
-			return false;
-		}
-		$login_http = $signature->getLogin();
-		$this->addActionOK("Le document a été envoyé au parapheur électronique via le login $login_http ");
-		return true;			
-	}
-	
-}
+/**
+ * Class IParapheurEnvoieDocumentASigner
+ * @deprecated PA 3.0 - utiliser la classe SignatureEnvoie à la place
+ */
+class IParapheurEnvoieDocumentASigner extends SignatureEnvoie { /** Nothing to do */}
