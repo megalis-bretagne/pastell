2e83eeeebb2da3a4c960683c527b2c82902a850a
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-07-23 09:23:45 +0200

    Ménage

--- module/pdf-generique/action/PDFGeneriqueSAEVerif.class.php
+++ module/pdf-generique/action/PDFGeneriqueSAEVerif.class.php
@@ -3,7 +3,7 @@
 require_once __DIR__."/../../../connecteur-type/SAE/SAEVerifier.class.php";
 /**
  * Class PDFGeneriqueSAEVerif
- * @deprecated PA 2.1
+ * @deprecated PA 3.0
  *
  */
 class PDFGeneriqueSAEVerif extends SAEVerifier { /* PDFGeneriqueSAEVerif and SAEVerifier are the same */ }
