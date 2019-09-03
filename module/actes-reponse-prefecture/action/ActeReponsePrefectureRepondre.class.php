78e5577bc24b3aa98ecaeac9b5ab2d2c0aa6d468
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-09-03 16:25:18 +0200

    following codereview

--- module/actes-reponse-prefecture/action/ActeReponsePrefectureRepondre.class.php
+++ module/actes-reponse-prefecture/action/ActeReponsePrefectureRepondre.class.php
@@ -6,7 +6,8 @@ class ActeReponsePrefectureRepondre extends ActionExecutor
     public function go()
     {
         $this->getDonneesFormulaire()->setData('repondre', true);
+        $this->redirect(sprintf("Document/edition?id_d=%s&id_e=%s&page=1", $this->id_d, $this->id_e));
+
         return true;
     }
-
 }
