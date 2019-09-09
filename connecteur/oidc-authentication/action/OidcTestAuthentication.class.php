c3b94e9c258387ab2cef954c249315941a5abf94
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-09-09 15:06:32 +0200

    create connector oidc-authentication

---
+++ connecteur/oidc-authentication/action/OidcTestAuthentication.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class OidcTestAuthentication extends ActionExecutor
+{
+
+    /**
+     * @return bool
+     * @throws Exception
+     */
+    public function go()
+    {
+        $oidc = $this->getMyConnecteur();
+        $login = $oidc->authenticate(SITE_BASE . "/Connexion/externalAuthentication?id_ce={$this->id_ce}");
+
+        if (!$login) {
+            $this->setLastMessage('Aucune session en cours');
+            return false;
+        }
+        $this->setLastMessage("Authentifié avec le login : $login");
+        return true;
+    }
+}
---
+++ connecteur/oidc-authentication/action/OidcTestLogout.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class OidcTestLogout extends ActionExecutor
+{
+
+    /**
+     * @return bool
+     * @throws Exception
+     */
+    public function go()
+    {
+        $oidc = $this->getMyConnecteur();
+        $oidc->logout(SITE_BASE);
+        $this->setLastMessage('Déconnecté avec succès');
+        return true;
+    }
+}
