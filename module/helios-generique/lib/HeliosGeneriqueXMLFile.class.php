6bb0bf5ae70f25d521b81eb9dab7b263cf608f84
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-09-10 08:58:11 +0200

    Commit pirate pour assurer une non régression

---
+++ module/helios-generique/lib/HeliosGeneriqueXMLFile.class.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__."/../../../connecteur-type/TdT/lib/PESV2XMLFile.class.php";
+
+/**
+ * Class HeliosGeneriqueXMLFile
+ * @deprecated PA V3 use PESV2XMLFile instead
+ */
+class HeliosGeneriqueXMLFile extends PESV2XMLFile
+{
+
+	/**
+	 * @param $file_path
+	 * @return SimpleXMLElement
+	 * @throws Exception
+	 * @deprecated PA V3 use getSimpleXMLFromFile instead
+	 */
+	public function getPESAllerAsSimpleXML($file_path){
+		return $this->getSimpleXMLFromFile($file_path);
+	}
+}
