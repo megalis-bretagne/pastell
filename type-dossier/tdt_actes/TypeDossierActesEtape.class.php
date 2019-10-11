47ba7e1323052e0970284ca1444e645d193af990
iruiz <ines.ruiz@libriciel.coop>
2019-10-11 10:48:54 +0200

    studio: correction de l'étape envoi_tdt_actes #916

--- type-dossier/tdt_actes/TypeDossierActesEtape.class.php
+++ type-dossier/tdt_actes/TypeDossierActesEtape.class.php
@@ -4,22 +4,26 @@ class TypeDossierActesEtape implements TypeDossierEtapeSetSpecificInformation
 {
 
 	public function setSpecificInformation(TypeDossierEtapeProperties $typeDossierEtape, array $result, StringMapper $stringMapper): array
-	{
-		$type_piece_action = $stringMapper->get('type-piece');
-		$send_tdt = $stringMapper->get('send-tdt');
-		$verif_tdt = $stringMapper->get('verif-tdt');
+    {
+        $type_piece_action = $stringMapper->get('type-piece');
+        $send_tdt = $stringMapper->get('send-tdt');
+        $verif_tdt = $stringMapper->get('verif-tdt');
         $onglet_name = $stringMapper->get('Acte');
 
-		$result['action'][$type_piece_action]['connecteur-type-mapping']['arrete'] = $typeDossierEtape->specific_type_info['fichier_acte'];
-		$result['action'][$type_piece_action]['connecteur-type-mapping']['autre_document_attache'] = $typeDossierEtape->specific_type_info['fichier_annexe'];
-
-		$result['action'][$send_tdt]['connecteur-type-mapping']['arrete'] = $typeDossierEtape->specific_type_info['fichier_acte'];
-		$result['action'][$send_tdt]['connecteur-type-mapping']['autre_document_attache'] = $typeDossierEtape->specific_type_info['fichier_annexe'];
-		$result['action'][$send_tdt]['connecteur-type-mapping']['objet'] = $typeDossierEtape->specific_type_info['objet_acte'];
-
-		$result['action'][$verif_tdt]['connecteur-type-mapping']['arrete'] = $typeDossierEtape->specific_type_info['fichier_acte'];
-		$result['action'][$verif_tdt]['connecteur-type-mapping']['autre_document_attache'] = $typeDossierEtape->specific_type_info['fichier_annexe'];
-		$result['action'][$verif_tdt]['connecteur-type-mapping']['objet'] = $typeDossierEtape->specific_type_info['objet_acte'];
+        if (!empty($typeDossierEtape->specific_type_info['fichier_acte'])) {
+            $result['action'][$type_piece_action]['connecteur-type-mapping']['arrete'] = $typeDossierEtape->specific_type_info['fichier_acte'];
+            $result['action'][$send_tdt]['connecteur-type-mapping']['arrete'] = $typeDossierEtape->specific_type_info['fichier_acte'];
+            $result['action'][$verif_tdt]['connecteur-type-mapping']['arrete'] = $typeDossierEtape->specific_type_info['fichier_acte'];
+        }
+        if (!empty($typeDossierEtape->specific_type_info['fichier_annexe'])) {
+            $result['action'][$type_piece_action]['connecteur-type-mapping']['autre_document_attache'] = $typeDossierEtape->specific_type_info['fichier_annexe'];
+            $result['action'][$send_tdt]['connecteur-type-mapping']['autre_document_attache'] = $typeDossierEtape->specific_type_info['fichier_annexe'];
+            $result['action'][$verif_tdt]['connecteur-type-mapping']['autre_document_attache'] = $typeDossierEtape->specific_type_info['fichier_annexe'];
+        }
+        if (!empty($typeDossierEtape->specific_type_info['objet_acte'])) {
+            $result['action'][$send_tdt]['connecteur-type-mapping']['objet'] = $typeDossierEtape->specific_type_info['objet_acte'];
+            $result['action'][$verif_tdt]['connecteur-type-mapping']['objet'] = $typeDossierEtape->specific_type_info['objet_acte'];
+        }
 
         if ($typeDossierEtape->requis){
             unset($result['page-condition'][$onglet_name]);
