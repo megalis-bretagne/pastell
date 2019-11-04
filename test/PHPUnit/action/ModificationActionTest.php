37265a4d47dc950718e559ae909d22bb0864ea08
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-11-04 20:10:13 +0100

    L'API ne répondait pas correctement lors de la modification de cheminement sur helios-generique #952

--- test/PHPUnit/action/ModificationActionTest.php
+++ test/PHPUnit/action/ModificationActionTest.php
@@ -25,4 +25,12 @@ class ModificationActionTest extends PastellTestCase
         $donnesFormulaire = $this->getDonneesFormulaireFactory()->get($document['id_d']);
         $this->assertSame('test required field', $donnesFormulaire->get('test2'));
     }
+
+    public function testDontRedirectOnAPICall(){
+		$document = $this->createDocument('helios-generique');
+		$this->expectOutputString("");
+		$result = $this->getInternalAPI()->patch("/Entite/1/Document/{$document['id_d']}",['envoi_sae'=>1]);
+		$this->assertEquals(1,$result['content']['data']['envoi_sae']);
+	}
+
 }
