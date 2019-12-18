c36d816a0b2b78d62e5bc2f41f64be53245685b5
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-12-18 15:59:05 +0100

    add tdt cancellation on studio modules

---
+++ connecteur-type/TdT/TdTAnnulation.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class TdTAnnulation extends ConnecteurTypeActionExecutor
+{
+
+    /**
+     * @return bool
+     * @throws NotFoundException
+     * @throws UnrecoverableException
+     */
+    public function go()
+    {
+        $tedetis_transaction_id = $this->getMappingValue('tedetis_transaction_id');
+        $tedetis_annulation_id =  $this->getMappingValue('tedetis_annulation_id');
+        $has_annulation =  $this->getMappingValue('has_annulation');
+        $annulation_tdt =  $this->getMappingValue('annulation-tdt');
+
+        /** @var TdtConnecteur $tdT */
+        $tdT = $this->getConnecteur('TdT');
+
+        $tedetis_transaction_id_element = $this->getDonneesFormulaire()->get($tedetis_transaction_id);
+
+        $id_annulation_transaction = $tdT->annulationActes($tedetis_transaction_id_element);
+        if (!$id_annulation_transaction) {
+            $this->setLastMessage($tdT->getLastError());
+            return false;
+        }
+        $this->getDonneesFormulaire()->setData($tedetis_annulation_id, $id_annulation_transaction);
+        $this->getDonneesFormulaire()->setData($has_annulation, true);
+
+        $this->addActionOK("Une notification d'annulation a été envoyée au contrôle de légalité");
+        $this->notify(
+            $annulation_tdt,
+            $this->type,
+            "Une notification d'annulation a été envoyée au contrôle de légalité"
+        );
+
+        return true;
+    }
+}
--- module/actes-generique/action/TedetisRecupAnnulation.class.php
+++ module/actes-generique/action/TedetisRecupAnnulation.class.php
@@ -1,45 +1,8 @@
 <?php
 
-class TedetisRecupAnnulation extends ActionExecutor
+/**
+ * @deprecated Use TdTRecupAnnulation instead
+ */
+class TedetisRecupAnnulation extends TdTRecupAnnulation
 {
-
-    public function go()
-    {
-        $tdT = $this->getConnecteur("TdT");
-        
-        if (!$tdT) {
-            throw new Exception("Aucun Tdt disponible");
-        }
-        
-        $tedetis_transaction_id = $this->getDonneesFormulaire()->get('tedetis_annulation_id');
-        
-        $actionCreator = $this->getActionCreator();
-        if (! $tedetis_transaction_id) {
-            $message = "Une erreur est survenue lors de l'envoi à " . $tdT->getLogicielName();
-            $this->setLastMessage($message);
-            $actionCreator->addAction($this->id_e, 0, 'tdt-error', $message);
-            $this->notify('tdt-error', $this->type, $message);
-            return false;
-        }
-            
-        try {
-            $status = $tdT->getStatus($tedetis_transaction_id);
-        } catch (Exception $e) {
-            $message = "Echec de la récupération des informations : " .  $e->getMessage();
-            $this->setLastMessage($message);
-            return false;
-        }
-        if ($status != TdtConnecteur::STATUS_ACQUITTEMENT_RECU) {
-            $this->setLastMessage("La transaction d'annulation a comme statut : " . TdtConnecteur::getStatusString($status));
-            return true;
-        }
-        $actionCreator->addAction($this->id_e, 0, 'annuler-tdt', "L'acte a été annulé par le contrôle de légalité");
-        
-        $this->getDonneesFormulaire()->setData('date_ar_annulation', $tdT->getDateAR($tedetis_transaction_id));
-        
-        $message = "L'acquittement pour l'annulation de l'acte a été reçu.";
-        $this->notify('annuler-tdt', $this->type, $message);
-        $this->setLastMessage($message);
-        return true;
-    }
 }
