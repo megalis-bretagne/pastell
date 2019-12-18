578ff8eeb358a2fd1b5987a9251f6fedd1c85feb
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2019-12-18 15:55:39 +0100

    fix failing test

--- test/PHPUnit/FileUploaderMock.class.php
+++ test/PHPUnit/FileUploaderMock.class.php
@@ -1,22 +1,21 @@
 <?php
+
 class FileUploaderMock extends FileUploader
 {
 
     private $files;
-
-
 
     public function setFiles($files)
     {
         $this->files = $files;
     }
 
-    public function getFilePath($filename)
+    public function getFilePath($filename, $num_file = 0)
     {
         throw new Exception("Not implemented");
     }
 
-    public function getName($filename)
+    public function getName($filename, $num_file = 0)
     {
         return $filename;
     }
@@ -26,12 +25,12 @@ class FileUploaderMock extends FileUploader
         throw new Exception("Not implemented");
     }
 
-    public function getFileContent($form_name)
+    public function getFileContent($form_name, $num_file = 0)
     {
         return $this->files[$form_name];
     }
 
-    public function save($filename, $save_path)
+    public function save($filename, $save_path, $num_file = 0)
     {
         throw new Exception("Not implemented");
     }
