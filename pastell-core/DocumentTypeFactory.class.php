d926b4896cfde74759ffb90470450b5e5fdc4ac9
iruiz <ines.ruiz@libriciel.coop>
2019-12-19 15:24:03 +0100

    Recherche avancée: Il n'y avait que les types de dossier génériques qui apparaissaient #983

--- pastell-core/DocumentTypeFactory.class.php
+++ pastell-core/DocumentTypeFactory.class.php
@@ -8,6 +8,7 @@ class DocumentTypeFactory
     
     private $connecteurDefinitionFiles;
     private $fluxDefinitionFiles;
+    private $allType;
     
     public function __construct(
         ConnecteurDefinitionFiles $connecteurDefinitionFiles,
@@ -60,10 +61,8 @@ class DocumentTypeFactory
     
     public function getAllType()
     {
-        static $result;
-        
-        if ($result) {
-            return $result;
+        if ($this->allType) {
+            return $this->allType;
         }
         $all_type = array();
         foreach ($this->fluxDefinitionFiles->getAll() as $id_flux => $properties) {
@@ -78,7 +77,8 @@ class DocumentTypeFactory
         
         $result[DocumentType::TYPE_FLUX_DEFAULT] =  $all_type[DocumentType::TYPE_FLUX_DEFAULT];
         unset($all_type[DocumentType::TYPE_FLUX_DEFAULT]);
-        return $result + $all_type;
+        $this->allType = $result + $all_type;
+        return $this->allType;
     }
     
     public function isSuperTypePresent($type)
