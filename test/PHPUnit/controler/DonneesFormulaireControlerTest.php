15170df3fcaa466b66f11ae951ce1bd90f11f1f1
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2020-01-22 16:14:54 +0100

    encode in utf8 the filename for browsers that supports it

--- test/PHPUnit/controler/DonneesFormulaireControlerTest.php
+++ test/PHPUnit/controler/DonneesFormulaireControlerTest.php
@@ -13,7 +13,7 @@ class DonneesFormulaireControlerTest extends ControlerTestCase
         /* ZipArchive ca marche pas avec le workspace émulé en mémoire */
         $this->getObjectInstancier()->setInstance('workspacePath', $tmp_folder);
 
-        $info = $this->getInternalAPI()->post("entite/1/document", array('type' => 'actes-generique'));
+        $info = $this->createDocument('actes-generique');
         $id_d = $info['id_d'];
         $donneesFormulaire = $this->getDonneesFormulaireFactory()->get($id_d);
         $donneesFormulaire->addFileFromCopy(
@@ -26,15 +26,14 @@ class DonneesFormulaireControlerTest extends ControlerTestCase
             'autre_document_attache',
             __DIR__ . "/../fixtures/vide.pdf",
             __DIR__ . "/../fixtures/test_extract_zip_structure/7756W3_9/7756_Bordereau_versement.pdf"
-        )
-        ;
+        );
 
         /** @var DonneesFormulaireControler $documentControler */
         $documentControler = $this->getControlerInstance(DonneesFormulaireControler::class);
 
         $this->setGetInfo(['id_e' => 1,'id_d' => $info['id_d'],'field' => 'autre_document_attache']);
 
-        $this->expectOutputRegex("#Content-disposition: attachment; filename=\"fichier-1-$id_d-autre_document_attache.zip\"#");
+        $this->expectOutputRegex("#Content-disposition: attachment; filename\*=UTF-8''fichier-1-$id_d-autre_document_attache.zip; filename=fichier-1-$id_d-autre_document_attache.zip#");
         $documentControler->downloadAllAction();
         $tmpFolder->delete($tmp_folder);
     }
