f43d956fbedb73c38354ead7521e7d716a638755
Eric Pommateau <eric.pommateau@libriciel.coop>
2020-03-10 16:55:10 +0100

    Correction

--- test/PHPUnit/action/ModificationActionTest.php
+++ test/PHPUnit/action/ModificationActionTest.php
@@ -33,4 +33,42 @@ class ModificationActionTest extends PastellTestCase
         $result = $this->getInternalAPI()->patch("/Entite/1/Document/{$document['id_d']}", ['envoi_sae' => 1]);
         $this->assertEquals(1, $result['content']['data']['envoi_sae']);
     }
+
+    /**
+     * @throws NotFoundException
+     */
+    public function testModifActionWhenFieldIsNotEditable()
+    {
+        $id_d = $this->createDocument('actes-generique')['id_d'];
+
+        $actionChange = $this->getObjectInstancier()->getInstance(ActionChange::class);
+
+        $actionChange->addAction($id_d, 1, 0, 'recu-iparapheur', 'test');
+
+        $this->getInternalAPI()->post(
+            "/Entite/1/Document/$id_d/file/arrete",
+            ['file_content' => 'test', 'file_name' => 'foo']
+        );
+
+        $donneesFormulaire = $this->getDonneesFormulaireFactory()->get($id_d);
+
+        $this->assertEmpty($donneesFormulaire->getFileContent('arrete'));
+    }
+
+    /**
+     * @throws NotFoundException
+     */
+    public function testModifActionWhenFieldIsEditable()
+    {
+        $id_d = $this->createDocument('actes-generique')['id_d'];
+
+        $this->getInternalAPI()->post(
+            "/Entite/1/Document/$id_d/file/arrete",
+            ['file_content' => 'test', 'file_name' => 'foo']
+        );
+
+        $donneesFormulaire = $this->getDonneesFormulaireFactory()->get($id_d);
+
+        $this->assertEquals('test', $donneesFormulaire->getFileContent('arrete'));
+    }
 }
