30f91ae336060a51f4dd970a6f6c7ebf68ad1b1c
Eric Pommateau <eric.pommateau@libriciel.coop>
2020-04-27 08:54:23 +0200

    add test

--- test/PHPUnit/connecteur/libersign/LibersignTest.php
+++ test/PHPUnit/connecteur/libersign/LibersignTest.php
@@ -15,6 +15,24 @@ class LibersignTest extends \PHPUnit\Framework\TestCase
             file_get_contents(__DIR__ . "/fixtures/Signature.xml"),
             true
         );
-        $this->assertStringEqualsFile(__DIR__ . "/fixtures/fichier_signe.xml", $xml);
+        $this->assertXmlStringEqualsXmlFile(
+            __DIR__ . "/fixtures/fichier_signe.xml",
+            $xml
+        );
+    }
+
+    public function testInjectWithAccent()
+    {
+        $libersign = new Libersign();
+
+        $xml = $libersign->injectSignaturePES(
+            __DIR__ . "/fixtures/HELIOS_SIMU_ALR2_1529593767_1834011863.xml",
+            base64_encode("<test><test><bla>école</bla></test></test>"),
+            false
+        ) ;
+        $this->assertXmlStringEqualsXmlFile(
+            __DIR__ . "/fixtures/fichier_signe_avec_accent.xml",
+            $xml
+        );
     }
 }
