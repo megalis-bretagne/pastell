f117e0ffaaaf61db754d934e8e51bc3cc0a1eb6f
Eric Pommateau <eric.pommateau@libriciel.coop>
2020-08-28 16:52:07 +0200

    following code review

---
+++ test/PHPUnit/TypeDossierLoaderTestTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . "/pastell-core/type-dossier/TypeDossierLoader.class.php";
+
+trait TypeDossierLoaderTestTrait
+{
+
+    /**
+     * @param $type_dossier_file_path
+     * @throws TypeDossierException
+     */
+    protected function loadTypeDossier($type_dossier_file_path)
+    {
+        $this->getObjectInstancier()
+            ->getInstance(TypeDossierLoader::class)
+            ->createTypeDossierFromFilepath($type_dossier_file_path);
+    }
+
+    protected function unloadTypeDossier()
+    {
+        $this->getObjectInstancier()
+            ->getInstance(TypeDossierLoader::class)
+            ->unload();
+    }
+
+    /**
+     * @return ObjectInstancier
+     */
+    abstract public function getObjectInstancier();
+}
