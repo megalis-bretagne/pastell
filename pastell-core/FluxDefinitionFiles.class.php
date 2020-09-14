5ab94a2d8d240eac8ab292b19b947c7a980a96b4
iruiz <ines.ruiz@libriciel.coop>
2020-09-14 08:28:35 +0200

    les flux inactifs ne sont plus visibles (il manque contrôle des droits et API)

--- pastell-core/FluxDefinitionFiles.class.php
+++ pastell-core/FluxDefinitionFiles.class.php
@@ -1,30 +1,47 @@
 <?php
 
+use Pastell\Service\Pack\PackService;
+
 //Chargement des fichier definition.yml dans les modules
 class FluxDefinitionFiles
 {
-    
-    public const DEFINITION_FILENAME = "definition.yml";
 
+    public const DEFINITION_FILENAME = "definition.yml";
     public const PASTELL_ALL_FLUX_CACHE_KEY = "pastell_all_flux";
 
     private $extensions;
     private $yml_loader;
+    private $packService;
     private $memoryCache;
     private $cache_ttl_in_seconds;
-    
+
     public function __construct(
         Extensions $extensions,
         YMLLoader $yml_loader,
+        PackService $packService,
         MemoryCache $memoryCache,
         $cache_ttl_in_seconds
     ) {
         $this->extensions = $extensions;
         $this->yml_loader = $yml_loader;
+        $this->packService = $packService;
         $this->memoryCache = $memoryCache;
         $this->cache_ttl_in_seconds = $cache_ttl_in_seconds;
     }
-    
+
+    /**
+     * @param array $flux_definition
+     * @return bool
+     */
+    private function isEnabledFlux(array $flux_definition = []): bool
+    {
+        $restriction_pack = $flux_definition[DocumentType::RESTRICTION_PACK] ?? [];
+        if (! $this->packService->restrictionHasEnabledPack($restriction_pack)) {
+            return false;
+        }
+        return true;
+    }
+
     public function getAll()
     {
         $result = $this->memoryCache->fetch(self::PASTELL_ALL_FLUX_CACHE_KEY);
@@ -37,7 +54,9 @@ class FluxDefinitionFiles
             $file_config = $module_path . "/" . self::DEFINITION_FILENAME;
             $config = $this->yml_loader->getArray($file_config);
             $id_flux = basename(dirname($file_config));
-            $result[$id_flux] = $config;
+            if ($this->isEnabledFlux($config)) {
+                $result[$id_flux] = $config;
+            }
         }
         uasort($result, array($this,"compareFluxDefinition"));
         $this->memoryCache->store(
@@ -55,7 +74,7 @@ class FluxDefinitionFiles
 
         return strcasecmp($str1, $str2);
     }
-    
+
     public function getInfo($id_flux)
     {
         return $this->yml_loader->getArray($this->getDefinitionPath($id_flux));
