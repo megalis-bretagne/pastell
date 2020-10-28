ce5769ab40725367069c91ddf1fe5718e704aac1
Eric Pommateau <eric.pommateau@libriciel.coop>
2020-10-28 15:38:09 +0100

    add possibility to use SedaGenerique in old flux

--- connecteur/seda-generique/SedaGenerique.class.php
+++ connecteur/seda-generique/SedaGenerique.class.php
@@ -6,8 +6,9 @@ use Twig\Error\SyntaxError;
 
 require_once(PASTELL_PATH . "/connecteur/seda-ng/lib/FluxData.class.php");
 require_once(PASTELL_PATH . "/connecteur/seda-ng/lib/FluxDataTest.class.php");
+require_once(PASTELL_PATH . "/connecteur/seda-ng/SedaNG.class.php");
 
-class SedaGenerique extends SEDAConnecteur
+class SedaGenerique extends SedaNG
 {
     /** @var DonneesFormulaire */
     private $connecteurConfig;
