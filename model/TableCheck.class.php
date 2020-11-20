4df835668e36cf9db7937c2a73521fc172a6cd91
Eric Pommateau <eric.pommateau@libriciel.coop>
2020-11-20 17:27:41 +0100

    La vérification des tables crashées était trop gourmande en ressources et a donc été corrigée #1163

--- model/TableCheck.class.php
+++ model/TableCheck.class.php
@@ -2,21 +2,9 @@
 
 class TableCheck extends SQL
 {
-
     public function getTablesMarkedAsCrashed(): array
     {
-        $tables_list = [];
-        $sql = "SHOW TABLES";
-        $col = $this->queryOneCol($sql);
-        $all_table = implode(',', $col);
-
-        $sql = "CHECK table $all_table";
-        $all_error = $this->query($sql);
-        foreach ($all_error as $error) {
-            if ($error['Msg_type'] == 'error') {
-                $tables_list[$error['Table']] = true ;
-            }
-        }
-        return array_keys($tables_list);
+        $sql = "SHOW TABLE STATUS WHERE comment != ''";
+        return $this->query($sql) ?: [];
     }
 }
