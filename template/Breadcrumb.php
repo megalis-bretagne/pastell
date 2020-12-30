8f2f062436d963292874be34f90e230fa0ea572a
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2020-12-30 16:10:48 +0100

    breadcrumbs fix #1072

--- template/Breadcrumb.php
+++ template/Breadcrumb.php
@@ -11,7 +11,7 @@
 
     <?php if ($display_entite_racine) : ?>
         <li>
-            <a href='<?php echo $navigation_url ?>'>Entité Racine</a>
+            <strong>Entité Racine</strong>
         </li>
         <li>
             <span class="divider">/</span>
@@ -19,7 +19,10 @@
     <?php endif; ?>
 
     <?php if (isset($navigation)) : ?>
-        <?php foreach ($navigation as $nav) :
+        <?php
+        $parentEntityId = 0;
+
+        foreach ($navigation as $nav) :
             $formId = 'bc_form_' . $nav['id_e'];
             $idSelect = 'select2_id_e_bc_' . $nav['id_e'];
             $idSelectSubmit = $idSelect . '_submit';
@@ -29,9 +32,9 @@
             <li>
                 <strong><?php hecho($nav['name']) ?></strong>
             </li>
-            <?php endif; ?>
+        <?php endif; ?>
 
-            <?php if (!$nav['is_root']) : ?>
+        <?php if (!$nav['is_root']) : ?>
             <li>
                 <form action='<?php echo $navigation_url ?>' method='get' id="<?php hecho($formId); ?>">
 
@@ -50,23 +53,37 @@
                 </form>
 
             </li>
-                <?php if ($nav['has_children']) : ?>
+        <?php if ($nav['has_children']) : ?>
             <li>
                 <span class="divider">/</span>
             </li>
-                <?php endif; ?>
+        <?php endif; ?>
             <script>
                 $(document).ready(function () {
                     $("#<?php hecho($idSelectSubmit); ?>").hide();
                     $("#<?php hecho($idSelect); ?>").change(function () {
                         $(this).parents("form").submit();
                     });
+
+                    $('.select2_breadcrumb').on("select2:unselecting", function (e) {
+                        e.preventDefault();
+                        let currentEntityId = e.params.args.data.id;
+                        if (currentEntityId == <?php hecho($nav['id_e']); ?>) {
+                            let parentEntityFormId = 'bc_form_' + <?php hecho($parentEntityId); ?>;
+                            console.log(parentEntityFormId);
+                            if (parentEntityFormId in document.forms) {
+                                document.forms[parentEntityFormId].submit();
+                            } else {
+                                $(this).parents('form').submit();
+                            }
+                        }
+                    });
                 });
             </script>
-            <?php endif; ?>
+        <?php endif; ?>
 
 
-            <?php if ($nav['is_last'] && !empty($nav['children'])) : ?>
+        <?php if ($nav['is_last'] && !empty($nav['children'])) : ?>
             <li>
                 <span class="divider">/</span>
             </li>
@@ -91,8 +108,11 @@
                     });
                 });
             </script>
-            <?php endif; ?>
-        <?php endforeach; ?>
+        <?php endif; ?>
+            <?php
+            $parentEntityId = $nav['id_e'];
+        endforeach;
+        ?>
     <?php endif; ?>
 
 </ul>
