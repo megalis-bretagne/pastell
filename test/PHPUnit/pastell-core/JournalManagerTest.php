f43173cb2eb15f86ff714f47204c33dbe87b159d
Eric Pommateau <eric.pommateau@libriciel.coop>
2021-04-23 16:51:32 +0200

    Fix unit test

--- test/PHPUnit/pastell-core/JournalManagerTest.php
+++ test/PHPUnit/pastell-core/JournalManagerTest.php
@@ -8,25 +8,4 @@ class JournalManagerTest extends PastellTestCase
         $journalManager = $this->getObjectInstancier()->getInstance(JournalManager::class);
         $this->assertTrue($journalManager->purgeToHistorique());
     }
-
-    /**
-     * @throws Exception
-     */
-    public function testPurgeFailed()
-    {
-        $id_j = $this->getJournal()->addConsultation(1, "XYZ", 1);
-        $this->getSQLQuery()->queryOne("UPDATE journal SET date=? WHERE id_j=?", "1977-02-18", $id_j);
-
-        $this->getSQLQuery()->query("INSERT INTO journal_historique (id_j) VALUES ($id_j) ");
-
-        $journalManager = $this->getObjectInstancier()->getInstance(JournalManager::class);
-        $this->assertFalse($journalManager->purgeToHistorique());
-
-        $this->assertEquals(
-            "Problème sur la purge du journalSQLSTATE[23000]: " .
-            "Integrity constraint violation: 1062 Duplicate entry '$id_j' for key 'PRIMARY' " .
-            "- INSERT INTO journal_historique SELECT * FROM journal WHERE id_j=?|$id_j",
-            $this->getLogRecords()[3]['message']
-        );
-    }
 }
