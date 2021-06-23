602508dd0aee50afc858b4d33e688133c857fea3
iruiz <ines.ruiz@libriciel.coop>
2021-06-23 16:42:23 +0200

    Il manquait la possibilité de supprimer en état `transformation-error #1318

---
+++ type-dossier/transformation/TypeDossierTransformationEtape.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class TypeDossierTransformationEtape implements TypeDossierEtapeSetSpecificInformation
+{
+
+    public function setSpecificInformation(TypeDossierEtapeProperties $typeDossierEtape, array $result, StringMapper $stringMapper): array
+    {
+        $transformation_error = $stringMapper->get('transformation-error');
+        $result[DocumentType::ACTION]['supression'][Action::ACTION_RULE][Action::ACTION_RULE_LAST_ACTION][] = $transformation_error;
+        return $result;
+    }
+}
