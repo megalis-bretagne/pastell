62d523e4063944ed2cf454fcff1445b242b378f8
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2021-07-30 14:56:36 +0200

    add global action to select and entity

---
+++ connecteur-type/utilities/GetEntityList.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class GetEntityList extends ConnecteurTypeChoiceActionExecutor
+{
+
+    private const ENTITY_ID = 'entity_id';
+    private const ENTITY_LABEL = 'entity_label';
+    private const PAGE_TITLE = 'page_title';
+
+    /**
+     * @return bool
+     */
+    public function go()
+    {
+        $entityId = $this->getRecuperateur()->getInt(self::ENTITY_ID);
+        if ($entityId === EntiteSQL::ID_E_ENTITE_RACINE) {
+            $entityLabel = EntiteSQL::ENTITE_RACINE_DENOMINATION;
+        } else {
+            $entityLabel = $this->objectInstancier->getInstance(EntiteSQL::class)->getDenomination($entityId);
+        }
+        $this->getConnecteurProperties()->setData($this->getMappingValue(self::ENTITY_LABEL), $entityLabel);
+        $this->getConnecteurProperties()->setData($this->getMappingValue(self::ENTITY_ID), $entityId);
+        return true;
+    }
+
+    public function display()
+    {
+        $this->entityList = $this->objectInstancier
+            ->getInstance(RoleUtilisateur::class)
+            ->getArbreFille($this->id_u, 'entite:edition');
+
+        $this->selectedEntity = $this->getConnecteurProperties()->get($this->getMappingValue(self::ENTITY_ID));
+        $this->renderPage(
+            $this->getMappingValue(self::PAGE_TITLE),
+            __DIR__ . '/template/GetEntityList.php'
+        );
+        return true;
+    }
+
+    public function displayAPI()
+    {
+        return [];
+    }
+}
