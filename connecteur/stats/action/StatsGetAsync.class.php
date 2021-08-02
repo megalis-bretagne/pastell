6a59e981d6aa82d939ca13473bd094d4a39c5a69
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2021-08-02 15:45:06 +0200

    implements stats connector

---
+++ connecteur/stats/action/StatsGetAsync.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class StatsGetAsync extends ActionExecutor
+{
+
+    public function go()
+    {
+        $jobManager = $this->objectInstancier->getInstance(JobManager::class);
+        $jobManager->setJobForConnecteur(
+            $this->id_ce,
+            'get_stats',
+            'La récupération des statistiques va être déclenchée en tâche de fond'
+        );
+        $this->setLastMessage('Déclenchement asynchrone de la récupération des statistiques');
+        return true;
+    }
+}
---
+++ test/PHPUnit/connecteur/stats/StatsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+class StatsTest extends PastellTestCase
+{
+
+    /**
+     * @throws NotFoundException
+     * @throws Exception
+     */
+    public function testGetStatsFromRoot(): void
+    {
+        $this->createDocument('test');
+
+        $document = $this->createDocument('test', self::ID_E_SERVICE);
+        $this->getDonneesFormulaireFactory()
+            ->get($document['id_d'])
+            ->addFileFromData('fichier', 'file name', 'data');
+        $connector = $this->createConnector('stats', 'Stats', 0);
+        $connectorFormulaire = $this->getDonneesFormulaireFactory()->getConnecteurEntiteFormulaire($connector['id_ce']);
+        $connectorFormulaire->setTabData([
+            'entity_id' => '0',
+            'module_type' => 'test',
+            'include_children' => true,
+            'start_date' => date('Y-m-d'),
+            'end_date' => date('Y-m-d', strtotime('+1 day')),
+        ]);
+
+        $this->triggerActionOnConnector($connector['id_ce'], 'get_stats');
+
+        $test = $this->getDonneesFormulaireFactory()->getConnecteurEntiteFormulaire($connector['id_ce']);
+
+        $this->assertSame(
+            date('Y-m-d'),
+            $test->get('csv_generation_date')
+        );
+        $expectedCsvContent = <<<EOT
+id_e,Entité,Nombre,Taille,État,"État label"
+1,Bourg-en-Bresse,1,217B,creation,Créé
+2,CCAS,1,248B,creation,Créé
+
+EOT;
+        $this->assertSame($expectedCsvContent, $test->getFileContent('csv_file'));
+    }
+}
