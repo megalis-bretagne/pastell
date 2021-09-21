974cd2c9c3efbf1a8be2a7045982c254d1bff5a2
iruiz <ines.ruiz@libriciel.coop>
2021-09-21 14:51:51 +0200

    fix test #1346

--- src/Service/Connecteur/ConnecteurHashService.php
+++ src/Service/Connecteur/ConnecteurHashService.php
@@ -2,25 +2,33 @@
 
 namespace Pastell\Service\Connecteur;
 
+use DonneesFormulaireFactory;
 use Pastell\Helpers\StringHelper;
 
 class ConnecteurHashService
 {
     private $workspacePath;
+    private $donneesFormulaireFactory;
 
-    public function __construct(string $workspacePath)
-    {
+    public function __construct(
+        string $workspacePath,
+        DonneesFormulaireFactory $donneesFormulaireFactory
+    ) {
         $this->workspacePath = $workspacePath;
+        $this->donneesFormulaireFactory = $donneesFormulaireFactory;
     }
 
     /**
      * @param int $id_ce
      * @return string
+     * @throws \Exception
      */
     public function getHash(int $id_ce): string
     {
-        $hash_connecteur = hash_file("sha256", $this->workspacePath . "/connecteur_$id_ce.yml");
+        //pour s'assurer que le yml existe
+        $this->donneesFormulaireFactory->getConnecteurEntiteFormulaire($id_ce);
 
+        $hash_connecteur = hash_file("sha256", $this->workspacePath . "/connecteur_$id_ce.yml");
         $all_file = glob($this->workspacePath . "/connecteur_$id_ce.yml_*");
         foreach ($all_file as $connecteur_file) {
             $hash_connecteur .= hash_file("sha256", $connecteur_file);
