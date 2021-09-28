12bd6ccf8ad9efcd1eb9852fbc3ef25b30420104
Eric <epommate@gmail.com>
2021-09-28 08:38:52 +0200

    Following code review

--- connecteur/generateur-seda/template/SedaGeneriqueFillData.php
+++ connecteur/generateur-seda/template/SedaGeneriqueFillData.php
@@ -50,7 +50,7 @@
                     <p class="form_commentaire">
                         Un mot clé par ligne de la forme : "Contenu du mot-clé","KeywordReference","KeywordType"
                         <br/><br/>Attention, si un élement contient une virgule, il est nécessaire d'entourer l'expression par des "guillemets"
-                        <br/><br/>Les lignes sont lues comme des lignes CSV (<b>,</b> comme séparateur de champs, <b>"</b> comme clôture de champs et <b>\</b> comme caractère d'échappement)
+                        <br/><br/>L'ensemble du champ est analysé avec Twig puis, les lignes sont lues comme des lignes CSV (<b>,</b> comme séparateur de champs, <b>"</b> comme clôture de champs et <b>\</b> comme caractère d'échappement)
                         <br/><br/>Les mots clés sont mis dans le bordereau au niveau Archive - Keyword (seda 1.0)/ ArchiveUnit - Keyword (seda 2.1)
                     </p>
                 </th>
