3d5fdfc778e569523e82070c8310b1367141ae85
Eric Pommateau <eric.pommateau@libriciel.coop>
2022-03-15 17:37:49 +0100

    Fix phpstan

--- connecteur/FakeGED/FakeGED.class.php
+++ connecteur/FakeGED/FakeGED.class.php
@@ -2,11 +2,9 @@
 
 declare(strict_types=1);
 
-use JetBrains\PhpStorm\ArrayShape;
-
 class FakeGED extends GEDConnecteur
 {
-    #[ArrayShape(['fake-document' => 'string'])] public function send(DonneesFormulaire $donneesFormulaire): array
+    public function send(DonneesFormulaire $donneesFormulaire): array
     {
         return ['fake-document' => 'fake-id'];
     }
