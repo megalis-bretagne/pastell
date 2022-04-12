ceadc7b089e107e6220bc21bf62c2eb55d841573
Eric Pommateau <eric.pommateau@libriciel.coop>
2022-04-12 17:18:04 +0200

    Fix warning in connector selection

--- template/FluxEdition.php
+++ template/FluxEdition.php
@@ -72,10 +72,20 @@ aucun connecteur sélectionné
             <td>&nbsp;</td>
             <td>&nbsp;</td>
         </tr>
-    <?php foreach ($connecteur_disponible as $connecteur) : ?>
+    <?php foreach ($connecteur_disponible as $connecteur) :?>
         <tr>
             <td>
-                <input type='radio' name='id_ce' value='<?php hecho($connecteur['id_ce'])?>' <?php echo  $connecteur_info['id_ce'] == $connecteur['id_ce'] ? "checked='checked'" : ""?> />
+                <input
+                        type='radio'
+                        name='id_ce'
+                        value='<?php hecho($connecteur['id_ce'])?>'
+                        <?php
+                        if (
+                            isset($connecteur_info['id_ce']) && $connecteur_info['id_ce'] === $connecteur['id_ce']
+                        ) : ?>
+                            checked='checked'
+                        <?php endif; ?>
+                />
                 &nbsp;&nbsp;
                 <a href='<?php $this->url("Connecteur/edition?id_ce={$connecteur['id_ce']}")?>'><?php hecho($connecteur['libelle'])?></a>
             </td>
