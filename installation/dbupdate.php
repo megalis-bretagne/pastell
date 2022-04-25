8619649a68352d21131763f0190b77ecee69a80a
Eric Pommateau <eric.pommateau@libriciel.coop>
2022-04-25 17:38:48 +0200

    Delete deprecated scripts

--- installation/dbupdate.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//TODO à supprimer
-/*
- * @deprecated 3.1.2 - USE script/plateform-update/database-update.php instead
- */
-
-require_once __DIR__ . "/../script/plateform-update/database-update.php";
