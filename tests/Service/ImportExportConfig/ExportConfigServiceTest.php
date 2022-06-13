4ee0432de75a3fc5f427abaa43fdf8a7457c4678
Eric Pommateau <eric.pommateau@libriciel.coop>
2022-06-13 11:47:36 +0200

    Fix cs

--- tests/Service/ImportExportConfig/ExportConfigServiceTest.php
+++ tests/Service/ImportExportConfig/ExportConfigServiceTest.php
@@ -20,7 +20,7 @@ class ExportConfigServiceTest extends PastellTestCase
     public function testAll(): void
     {
         $fluxEntiteSQl = $this->getObjectInstancier()->getInstance(\FluxEntiteSQL::class);
-        $fluxEntiteSQl->deleteConnecteur(1,'fournisseur-invitation', 'mail-fournisseur-invitation');
+        $fluxEntiteSQl->deleteConnecteur(1, 'fournisseur-invitation', 'mail-fournisseur-invitation');
         $id_ce = $this->createConnector('test', "foo", 2)['id_ce'];
         $this->configureConnector($id_ce, [
             'champs1' => 'bar',
