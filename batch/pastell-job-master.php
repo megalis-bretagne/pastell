8c84616346c7f17e023c49e5c07f63dfba663a2f
Eric Pommateau <eric.pommateau@libriciel.coop>
2022-08-29 15:50:20 +0200

    Correctif

--- batch/pastell-job-master.php
+++ batch/pastell-job-master.php
@@ -1,12 +1,11 @@
 <?php
+
 $logger_system = "DAEMON";
 
 require_once __DIR__ . '/../init.php';
 
 $pastellDaemon = ObjectInstancierFactory::getObjetInstancier()->getInstance(PastellDaemon::class);
 
-pcntl_signal(SIGTERM, function() use ($pastellDaemon): void {
-    $pastellDaemon->stop();
-});
+pcntl_signal(SIGTERM, fn() => $pastellDaemon->stop());
 
 $pastellDaemon->jobMaster();
