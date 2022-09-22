0b490c6930fab67d5501fa7813cdeb39a29e1574
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2022-09-22 11:20:50 +0200

    add test

---
+++ tests/Legacy/LegacyRouteLoaderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pastell\Tests\Legacy;
+
+use Pastell\Legacy\LegacyRouteLoader;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Route;
+
+final class LegacyRouteLoaderTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testLoad(): void
+    {
+        $legacyRouteLoader = new LegacyRouteLoader();
+        $routes = $legacyRouteLoader->load('');
+
+        $this->assertCount(229, $routes);
+        $this->assertContainsOnly(Route::class, $routes);
+
+        foreach ($routes as $route) {
+            $this->assertSame(
+                'Pastell\Controller\LegacyController::loadLegacyScript',
+                $route->getDefault('_controller')
+            );
+        }
+    }
+}
