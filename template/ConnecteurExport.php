22869ce0edaed347a6cef614db0415aaafeec31a
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2022-10-19 09:31:54 +0200

    Ajout mot de passe généré par Pastell sur Connecteur/export

--- template/ConnecteurExport.php
+++ template/ConnecteurExport.php
@@ -3,10 +3,11 @@
 /**
  * @var Gabarit $this
  * @var int $id_ce
+ * @var string $password
  */
 ?>
 <div class="alert alert-info">
-    Le mot de passe demandé permet de protéger le contenu du connecteur.
+    Le mot de passe généré permet de protéger le contenu du connecteur.
     Il sera nécessaire pour importer à nouveau le connecteur sur un Pastell en version 3.1 ou ultérieur.
 </div>
 <div class="box">
@@ -14,54 +15,7 @@
     <form action='Connecteur/doExport' method='post'>
         <?php $this->displayCSRFInput() ?>
         <input type='hidden' name='id_ce' value='<?php hecho($id_ce)?>'>
-        <table class='table table-striped'>
-            <tr>
-                <th>
-                    <label for='password'>
-                        Mot de passe<span class='obl'>*</span>
-                    </label>
-                </th>
-                <td>
-                    <div class="input-group">
-                        <input
-                                id="password"
-                                type="password"
-                                class="form-control col-md-4 ls-box-input"
-                                name="password"
-                                value=''
-                                minlength="8"
-                                required
-                        />
-                        <div class="input-group-append">
-                            <span class="input-group-text"><i class="fa fa-eye-slash" onclick="switchInputType('password',this)"></i></span>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    <label for='password_check'>
-                        Mot de passe (vérification)<span class='obl'>*</span>
-                    </label>
-                </th>
-                <td>
-                    <div class="input-group">
-                        <input
-                                id="password_check"
-                                type="password"
-                                class="form-control col-md-4 ls-box-input"
-                                name="password_check"
-                                value=''
-                                minlength="8"
-                                required
-                        />
-                        <div class="input-group-append">
-                            <span class="input-group-text"><i class="fa fa-eye-slash" onclick="switchInputType('password_check',this)"></i></span>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </table>
+        <input type='hidden' name='password' value='<?php hecho($password)?>'>
 
         <a class='btn btn-outline-primary' href='Connecteur/edition?id_ce=<?php hecho($id_ce); ?>'>
             <i class="fa fa-times-circle"></i>&nbsp;Annuler
