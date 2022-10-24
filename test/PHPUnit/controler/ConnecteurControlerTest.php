14fc6d0b794a9580a4ee67193a1fce0042219b4d
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2022-10-24 09:55:54 +0200

    modification du test avec mise en cache du password

--- test/PHPUnit/controler/ConnecteurControlerTest.php
+++ test/PHPUnit/controler/ConnecteurControlerTest.php
@@ -7,6 +7,7 @@ use ParagonIE\Halite\Alerts\InvalidMessage;
 use ParagonIE\Halite\Alerts\InvalidSalt;
 use ParagonIE\Halite\Alerts\InvalidType;
 use Pastell\Service\Connecteur\ConnecteurActionService;
+use Symfony\Component\Security\Csrf\TokenGenerator\UriSafeTokenGenerator;
 
 class ConnecteurControlerTest extends ControlerTestCase
 {
@@ -123,10 +124,13 @@ class ConnecteurControlerTest extends ControlerTestCase
      */
     public function testDoExport(): void
     {
+        $id_ce = 11;
         $this->setPostInfo([
-            'id_ce' => 11,
-            'password' => '12345678'
+            'id_ce' => $id_ce
         ]);
+        $generator = new UriSafeTokenGenerator();
+        $password = $generator->generateToken();
+        $this->getObjectInstancier()->getInstance(MemoryCache::class)->store($id_ce . '_' . $this->connecteurControler->getId_u(), $password, 60);
 
         $this->expectOutputRegex('/Content-type: application\/json;*/');
         $this->connecteurControler->doExportAction();
