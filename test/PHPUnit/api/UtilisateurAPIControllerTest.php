0045ad00fe2f7950019868da0f5fc95ff75f5a3b
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2022-10-25 10:57:44 +0200

    Ajout test activer/desactiver, sur soi-meme et pas les droits

--- test/PHPUnit/api/UtilisateurAPIControllerTest.php
+++ test/PHPUnit/api/UtilisateurAPIControllerTest.php
@@ -2,6 +2,7 @@
 
 class UtilisateurAPIControllerTest extends PastellTestCase
 {
+
     public function testCreate()
     {
         $info = $this->getInternalAPI()->post(
@@ -185,5 +186,35 @@ class UtilisateurAPIControllerTest extends PastellTestCase
         $this->expectException(Exception::class);
         $this->expectExceptionMessage("L'utilisateur n'existe pas : {id_u=1}");
         $this->getInternalAPI()->get("utilisateur/1");
+    }
+
+    public function testPostActivateDeactivate(): void
+    {
+        $utilisateurActivated = $this->getInternalAPI()->get('utilisateur/2');
+        $utilisateurDeactivated = $this->getInternalAPI()->post('/utilisateur/2/deactivate');
+        $this->assertNotEquals($utilisateurActivated, $utilisateurDeactivated);
+
+        $utilisateurReactivated = $this->getInternalAPI()->post('/utilisateur/2/activate');
+        $this->assertEquals($utilisateurActivated, $utilisateurReactivated);
+    }
+
+    public function testPostDeactivateMyselfFail(): void
+    {
+        $this->expectException(UnrecoverableException::class);
+        $this->expectExceptionMessage('Vous ne pouvez pas désactiver votre compte utilisateur.');
+        $this->getInternalAPI()->post('/utilisateur/1/deactivate');
+    }
+
+    public function testPostDeactivateFailDroit(): void
+    {
+        $this->getObjectInstancier()->getInstance(UtilisateurCreator::class)
+            ->create('tester', 'tester', 'tester', 'tester@mail');
+        $this->getObjectInstancier()->getInstance(RoleSQL::class)
+            ->edit('utilisateur:lecture', 'lectureUser');
+        $this->getObjectInstancier()->getInstance(RoleUtilisateur::class)
+            ->addRole('3', 'lectureUser', '1');
+        $this->expectException(ForbiddenException::class);
+        $this->expectExceptionMessage('Acces interdit id_e=0, droit=utilisateur:edition,id_u=3');
+        $this->getInternalAPIAsUser('3')->post('/utilisateur/1/deactivate');
     }
 }
