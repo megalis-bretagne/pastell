049eefc8d97ce2daa4869b71cd5f60ecf083c64c
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2023-01-19 15:20:08 +0100

    add help url connector implementation

---
+++ connecteur/help-url/HelpUrlConnector.class.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+final class HelpUrlConnector extends Connecteur
+{
+    public function setConnecteurConfig(DonneesFormulaire $donneesFormulaire)
+    {
+        // Nothing to do
+    }
+}
