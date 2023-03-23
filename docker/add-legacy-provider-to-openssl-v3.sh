192a8e5517a5a2158d72e3b6b9ebd4d69c3657c0
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2023-03-23 10:53:56 +0100

    set openssl cipherstring level on runtime

--- docker/add-legacy-provider-to-openssl-v3.sh
+++ docker/add-legacy-provider-to-openssl-v3.sh
@@ -8,6 +8,10 @@ set -ex
 sed -i '/^default = default_sect/a legacy = legacy_sect' /etc/ssl/openssl.cnf
 sed -i '/^\[default_sect\]/a activate = 1' /etc/ssl/openssl.cnf
 
+sed -i '1i OPENSSL_CIPHER_STRING_DEFAULT_SECURITY_LEVEL = 2\
+OPENSSL_CIPHER_STRING_SECURITY_LEVEL = $ENV::OPENSSL_CIPHER_STRING_DEFAULT_SECURITY_LEVEL' /etc/ssl/openssl.cnf
+
+sed -i 's/^CipherString = DEFAULT:@SECLEVEL=2$/CipherString = DEFAULT:@SECLEVEL=\$ENV::OPENSSL_CIPHER_STRING_SECURITY_LEVEL/g' /etc/ssl/openssl.cnf
 
 echo "
 [legacy_sect]
