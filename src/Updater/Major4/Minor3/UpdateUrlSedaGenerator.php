26892f08c57b0436b89a0edd5688853c404e6d4c
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2023-05-24 11:08:23 +0200

    add updater class & update comment

---
+++ src/Updater/Major4/Minor3/UpdateUrlSedaGenerator.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pastell\Updater\Major4\Minor3;
+
+use Pastell\Updater\Version;
+
+final class UpdateUrlSedaGenerator implements Version
+{
+
+    public function update(): void
+    {
+        // TODO: Implement update() method.
+    }
+}
