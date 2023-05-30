c564c9ac2436e1138f9dcde76dd99dd5774343fc
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2023-05-30 09:55:10 +0200

    fix check field parapheur_circuit

--- connecteur-type/signature/SignatureEnvoie.php
+++ connecteur-type/signature/SignatureEnvoie.php
@@ -36,6 +36,15 @@ class SignatureEnvoie extends ConnecteurTypeActionExecutor
         $json_metadata = $this->getMappingValue('json_metadata');
         $iparapheur_dossier_id = $this->getMappingValue('iparapheur_dossier_id');
 
+        if (
+            !($donneesFormulaire->get($fast_parapheur_circuit_configuration)
+            || $donneesFormulaire->get($fast_parapheur_circuit))
+        ) {
+            $this->setLastMessage("Le formulaire est incomplet : le champ 'Circuit sur le parapheur' 
+            ou le champ 'Configuration du circuit à la volée' est obligatoire");
+            return false;
+        }
+
         $fileToSign = new FileToSign();
         $fileToSign->type = $donneesFormulaire->get($iparapheur_type_element);
         $fileToSign->sousType = $donneesFormulaire->get($iparapheur_sous_type_element);
