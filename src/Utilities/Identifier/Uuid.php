45ceb4c10747b861461d02308c3898a696926863
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2023-05-31 15:23:00 +0200

    fix lint

--- src/Utilities/Identifier/Uuid.php
+++ src/Utilities/Identifier/Uuid.php
@@ -6,7 +6,6 @@ namespace Pastell\Utilities\Identifier;
 
 class Uuid implements IdentifierInterface
 {
-
     public function generateId(): string
     {
         return uuid_create(UUID_TYPE_RANDOM);
