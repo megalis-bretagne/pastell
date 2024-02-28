dd0b4a2afd3b9a44d62a07f8ca647b9a8823bf97
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2024-02-28 16:57:48 +0100

    reformat js and css files

--- web/js/top.js
+++ web/js/top.js
@@ -1,26 +1,26 @@
-
 $(function () {
 
-  var offset = 300,
-      duration = 500,
-      top_section = $('.tothetop'),
-      toTopButton = $('a#top');
-  // showing and hiding button according to scroll amount (in pixels)
-  $(window).scroll(function () {
-    if( $(this).scrollTop() > offset ) {
-      $(top_section).fadeIn(duration);
-    } else{
-      $(top_section).fadeOut(duration);
+    var offset = 300,
+        duration = 500,
+        top_section = $('.tothetop'),
+        toTopButton = $('a#top');
+    // showing and hiding button according to scroll amount (in pixels)
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > offset) {
+            $(top_section).fadeIn(duration);
+        } else {
+            $(top_section).fadeOut(duration);
 
-    }});
+        }
+    });
 
-  // activate smooth scroll to top when clicking on the button
+    // activate smooth scroll to top when clicking on the button
 
-  $(toTopButton).click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-      scrollTop: 0
-    }, 700);
-  });
+    $(toTopButton).click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700);
+    });
 
 });
