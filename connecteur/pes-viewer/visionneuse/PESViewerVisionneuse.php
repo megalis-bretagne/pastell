72a0290b1b3c7afaec55e70eb75c1fc8f84866cf
iruiz <ines.ruiz@libriciel.coop>
2024-04-03 10:13:33 +0200

    review #2030

--- connecteur/pes-viewer/visionneuse/PESViewerVisionneuse.php
+++ connecteur/pes-viewer/visionneuse/PESViewerVisionneuse.php
@@ -6,11 +6,9 @@ use Pastell\Viewer\Viewer;
 
 class PESViewerVisionneuse implements Viewer
 {
-    private ConnecteurFactory $connecteurFactory;
-
-    public function __construct(ConnecteurFactory $connecteurFactory)
-    {
-        $this->connecteurFactory = $connecteurFactory;
+    public function __construct(
+        private readonly ConnecteurFactory $connecteurFactory
+    ) {
     }
 
     /**
